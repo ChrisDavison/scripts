65f61cc95570e82ca9af1a4d332b79dfea41d30f
Christopher Davison <c.jr.davison@gmail.com>
2015-05-04 18:39:46 +0100

    Python version.  Parses, but no other functionality

--- doibib.py
+++ doibib.py
@@ -6,38 +6,48 @@ import re
 # Regex
 # Instead, a list of keywords, which I prepend onto the regex
 # and then search with this?
-rEntr = re.compile('@(?P<entry_type>[a-zA-Z0-9]+){')
-keywords = ["author", "journal", "title", "year"]
-regexs =  {kw: "(?P<{}>{} = ({}) ".format(kw, kw, ".*?") for kw in keywords}
+rEntr = re.compile('@([a-zA-Z0-9]+)[{]')
+rKeyw = re.compile('@.*?[{](.*?),')
+regex = re.compile('(author|journal|title|year|url) = ([{].*?[}][,} ])')
 
-class bib_entry:
-    entry_type = ""
-    author = ""
-    title = ""
-    journal = ""
-    url = ""
+menu = """Bibliography Tool
+=================
+1) Search for an entry
+2) Add an entry
+3) List a certain detail
+4) Add via DOI number
+"""
 
 class bibliography:
     fn = ""
-    contents = None
+    entries = {}
     def __init__(self, fn):
-        # 
         self.fn = fn
-        self.__parse(fn)
+        self.parse_file(fn)
 
-    def __parse(self, filename):
+    def parse_file(self, filename):
         lines = [line for line in open(filename)]
         lines = lines[5:]
+
         for line in lines:
-            type = re.match(rEntr, line)
-            if type:
-                print(type.group('entry_type'))
-            out = []
-            for kw, regex in regexs.items():
-                m = re.match(regex, line)
-                if m:
-                    print(m.group(kw))
-            print(out)
+            try:
+                k, v = self.parse_entry(line)
+            except:
+                continue
+            self.entries[k] = v
+
+    def parse_entry(self, line):
+        try:
+            type = re.match(rEntr, line).groups()[0]
+            keyword = re.match(rKeyw, line).groups()[0]
+
+            matches = re.findall(regex, line) 
+            out = [match for match in matches]
+            out.append(('type', type))
+
+            return keyword, out
+        except:
+            return 
 
     def search(keyword):
         pass
@@ -53,6 +63,13 @@ class bibliography:
 
 def main():
     mybib = bibliography("library.bib")
+    for keyword, details in mybib.entries.items():
+        print(keyword)
+        for detail in details:
+            key, value = detail
+            print("{:>15} - {}".format(key, value))
+        print("-"*80)
+        print("-"*80)
 
 if __name__ == "__main__":
     main()
