95cd4666fbfc467e9e1cec7bf6ae48a94ad6f255
Christopher Davison <c.jr.davison@gmail.com>
2015-05-05 16:39:58 +0100

    Added argparse for add, edit and delete.  Not implemented functions yet

--- doibib.py
+++ doibib.py
@@ -11,6 +11,12 @@ parser.add_argument('--search',
 parser.add_argument('--print', action='store_true',
                     help='Print default bibliography')
 parser.add_argument('--doi', help='Get a bib entry from DOI')
+parser.add_argument('--add', action='store_true',
+                    help='Add an entry to the Bib')
+parser.add_argument('--delete', action='store_true',
+                    help='Remove an entry from the Bib')
+parser.add_argument('--edit', action='store_true',
+                    help='Edit a Bib entry')
 args = parser.parse_args()
 
 
@@ -26,8 +32,15 @@ def main():
         print(mybib)
     elif args.doi:
         mybib.from_doi(args.doi)
+    elif args.add:
+        mybib.add()
+    elif args.delete:
+        mybib.delete()
+    elif args.edit:
+        mybib.edit()
     else:
         parser.print_help()
+        print("\nPrecedence: Search, print, doi, add, delete, edit\n")
     mybib.write("library.bib")
 
 if __name__ == "__main__":
