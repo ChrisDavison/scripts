ec25c7f1ed6830b31c53bb277e657ddd73a91efb
Christopher Davison <c.jr.davison@gmail.com>
2016-08-24 13:04:54 +0100

    Use regex to filter a directory listing

--- osutils.py
+++ osutils.py
@@ -4,6 +4,7 @@
 Functions:
     dated_directory: Create a directory with todays date as name
     choose_from_dir: Choose from filtered files in a directory
+    listdir_matching_regex: Return filepath from directory matching regex
 """
 import os
 import datetime
@@ -68,3 +69,17 @@ def choose_from_dir(directory, **kwargs):
             break
     print()
     return os.path.join(directory, files[choice])
+
+
+def listdir_matching_regex(directory, regex):
+    "Given a regex, return matching files."
+    out = []
+    reg = re.compile(regex)
+    for f in os.listdir(directory):
+        m = reg.match(f)
+        has_group = '(' in regex and ')' in regex
+        if m and has_group:
+            out.append(m.group(1))
+        elif m:
+            out.append(m.group(0))
+    return out
