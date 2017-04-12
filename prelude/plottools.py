e8fce3effbc85bf38013d8416defd5865adaf9e4
Chris Davison <c.jr.davison@gmail.com>
2017-04-12 15:15:50 +0100

    Split creation of dated title

--- prelude/plottools.py
+++ prelude/plottools.py
@@ -18,14 +18,17 @@ def __fn_sanitise(s):
         s = s.replace(c,'')
     return s
 
-def dated_fig_dir_and_filename(notebook_name=None, title=None, ext='png'):
+def dated_title(title=None, ext='png'):
     fmtdate = dt.datetime.now().strftime("%Y%m%d-%H%M")
     title = __fn_sanitise(title).replace(" ", "-").lower()
+    return "{}--{}.{}".format(fmtdate, title, ext)
+
+def dated_fig_dir_and_filename(notebook_name=None, title=None, ext='png'):
     curdir = os.getcwd()
     plotdir = os.path.join(curdir, '..', '__PLOTS', __fn_sanitise(notebook_name))
     plotdir_abs = os.path.abspath(plotdir)
-    out = os.path.join(plotdir_abs, "{}--{}.{}".format(fmtdate, title, ext))
-    return plotdir, out
+    out = os.path.join(plotdir_abs, dated_title(title, ext))
+    return plotdir_abs, out
 
 def save_dated_fig(fig, *, notebook_name=None, title=None, ext='png'):
     """Save in ./../__PLOTS a file with date as prefix."""
