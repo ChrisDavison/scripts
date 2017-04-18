7dd4c30ea9a92dd3d72cd9aa56252bd164d6845c
Chris Davison <c.jr.davison@gmail.com>
2017-04-18 15:09:37 +0100

    Use module AND alias

--- prelude/__init__.py
+++ prelude/__init__.py
@@ -3,7 +3,6 @@ from .listop import *
 from .plottools import *
 from .utility import *
 from .indexable_generator import *
-
 from .plots import *
 
 import pandas_utils as pu
@@ -16,14 +15,14 @@ import argparse
 import os
 import re
 import sys
-import datetime as dt
+import datetime
 import json
 import shutil
 import matplotlib
 import logging
+import pprint
 
 from collections import defaultdict
-import pprint as pp
 
 from toolz.itertoolz import *
 from toolz.functoolz import *
@@ -33,12 +32,29 @@ from itertools import chain
 from os.path import join, expanduser
 
 # installed
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import dateutil.parser as dp
+import matplotlib.pyplot
+import numpy
+import pandas
+import dateutil.parser
+import scipy.fftpack
+import seaborn
 
-import scipy.fftpack as sfft
+from scipy.stats import gaussian_kde
+from sklearn import svm
+from sklearn.model_selection import train_test_split, cross_val_score
+import tensorflow
+import tflearn
+
+tf = tensorflow
+
+plt = matplotlib.pyplot
+np = numpy
+pd = pandas
+dp = dateutil.parser
+sfft = scipy.fftpack
+sns = seaborn
+dt = datetime
+pp = pprint
 
 # from sarascripts repo
 __homedir = expanduser('~'),
