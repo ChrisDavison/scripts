baf285c56b1e615ffb4fc3ee246477fa4509f44c
Chris Davison <c.jr.davison@gmail.com>
2017-05-04 22:39:41 +0100

    Some placeholder tests

--- conv_test.py
+++ conv_test.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import pytest
 from conv import *
 
 def test_lb_to_kg_negative():
@@ -15,3 +16,27 @@ def test_lb_to_kg_notnumber():
 
 def test_stonelb_to_kg_negative():
     assert stonelb_to_kg(-1) == None
+
+@pytest.mark.skip(reason="Not implemented")
+def test_stonelb_to_kg_0():
+    assert False == True
+
+@pytest.mark.skip(reason="Not implemented")
+def test_stonelb_to_kg_positive():
+    assert False
+
+@pytest.mark.skip(reason="Not implemented")
+def test_stonelb_to_kg_notnumber():
+    assert False
+
+@pytest.mark.skip(reason="Not implemented")
+def test_stonelb_to_kg_notnumber():
+    assert False
+
+@pytest.mark.skip(reason="Need full test suite for stone_to_kg")
+def test_stone_to_kg():
+    assert False
+
+@pytest.mark.skip(reason="Need full test suite for from_kg")
+def test_from_kg():
+    assert False
