2e2f4071ebd65626964b42a49a6d4c6a78b50642
Chris Davison <c.jr.davison@gmail.com>
2017-06-19 10:06:08 +0100

    Fixing pylint complaints

--- utility.py
+++ utility.py
@@ -1,38 +1,33 @@
-import datetime
-import dateutil
-import matplotlib.pyplot
-import numpy
+"""Various utility functions."""
 import os
 import re
 import functools
-
-dt = datetime
-dp = dateutil.parser
-plt = matplotlib.pyplot
-np = numpy
-
+import datetime
 from collections import defaultdict
 
+import dateutil.parser
+import numpy
 
-def choose_from_list(ls, msg="?: "):
+
+def choose_from_list(lst, msg="?: "):
     """Given a list, return a n entry.
 
     Useful to visually select from a large list of complex items."""
-    if len(ls) == 0:
+    if not lst:
         return None
-    if len(ls) == 1:
-        print("Only 1 item.  Chose: {}".format(ls[0]))
-        return ls[0]
-    for i, v in enumerate(ls):
-        print("{:3d}: {}".format(i, v))
+    if len(lst) == 1:
+        print("Only 1 item.  Chose: {}".format(lst[0]))
+        return lst[0]
+    for i, value in enumerate(lst):
+        print("{:3d}: {}".format(i, value))
     idx = input(msg)
-    return ls[int(idx)]
+    return lst[int(idx)]
 
-def choose_filtered(ls, condition, msg="?: "):
+def choose_filtered(lst, condition):
     """First filter a list, and then chose an entry from it."""
-    return choose_from_list(list(filter(condition, ls)))
+    return choose_from_list(list(filter(condition, lst)))
 
-def insert_into_timeseries(ts_host, ts_toEnter, things_toEnter, default=0):
+def insert_into_timeseries(ts_host, timeseries_to_enter, things_to_enter, default=0):
     """Insert -something- at nearest point in timeseries.
 
     Given [timestamps] and [(timestamp, thing)], insert
@@ -40,45 +35,45 @@ def insert_into_timeseries(ts_host, ts_toEnter, things_toEnter, default=0):
 
     Between timestamps, repeat current thing."""
 
-    out = np.full(len(ts_host), default)
+    out = numpy.full(len(ts_host), default)
 
     target = 0
-    t0, tn = ts_toEnter[target], ts_toEnter[target+1]
-    b0 = things_toEnter[target]
-    limit = len(ts_toEnter)
+    _, time_next = timeseries_to_enter[target], timeseries_to_enter[target+1]
+    thing_0 = things_to_enter[target]
+    limit = len(timeseries_to_enter)
 
-    for i, t in enumerate(ts_host):
-        if t >= tn:
+    for i, time in enumerate(ts_host):
+        if time >= time_next:
             target += 1
             if target < (limit - 1):
-                b0 = things_toEnter[target] # Current _thing_
-                tn = ts_toEnter[target+1] # Next time
-        out[i] = b0
+                thing_0 = things_to_enter[target] # Current _thing_
+                time_next = timeseries_to_enter[target+1] # Next time
+        out[i] = thing_0
     return out
 
-def timestamp_mean_duration(ts):
+def timestamp_mean_duration(timeseries):
     """Estimates mean duration from a list of timestamps.
 
     Arguments:
-    ts -- list of timestamps
+    timeseries -- list of timestamps
     """
-    if type(ts[0]) == type(""):
-        ts = list(map(dp.parse, ts))
-    pairs = zip(ts, ts[1:])
+    if isinstance(timeseries[0], list):
+        timeseries = list(map(dateutil.parser.parse, timeseries))
+    pairs = zip(timeseries, timeseries[1:])
     diffed = map(lambda z: (z[1] - z[0]).total_seconds(), pairs)
-    return np.mean(list(diffed))
+    return numpy.mean(list(diffed))
 
-def most_prevalent(ls):
+def most_prevalent(lst):
     """Return the most prevalent element in a list."""
     seen = defaultdict(int)
-    for value in ls:
+    for value in lst:
         seen[value] += 1
     largest = 0
     largest_key = ''
-    for k, v in seen.items():
-        if v > largest:
-            largest = v
-            largest_key = k
+    for key, value in seen.items():
+        if value > largest:
+            largest = value
+            largest_key = key
     return largest_key
 
 def listdir_matching_regex(directory, regex='.*.csv'):
@@ -87,17 +82,18 @@ def listdir_matching_regex(directory, regex='.*.csv'):
     By default, return CSVs from a directory."""
     out = []
     reg = re.compile(regex)
-    for f in os.listdir(directory):
-        m = reg.match(f)
+    for filename in os.listdir(directory):
+        match = reg.match(filename)
         has_group = '(' in regex and ')' in regex
-        if m and has_group:
-            out.append(m.group(1))
-        elif m:
-            out.append(m.group(0))
+        if match and has_group:
+            out.append(match.group(1))
+        elif match:
+            out.append(match.group(0))
     return out
 
-def choose_from_dir(directory, regex='.*.csv', msg='?: '):
-    return choose_from_list(listdir_matching_regex(directory, regex), msg)
+def choose_from_dir(directory, regex='.*.csv'):
+    """Given a directory, enumerate values and return chosen value"""
+    return choose_from_list(listdir_matching_regex(directory, regex))
 
 def dated_directory(root, suffix=None):
     """Create a directory with todays date and optional suffix.
@@ -115,7 +111,7 @@ def dated_directory(root, suffix=None):
     today_date_str = today.strftime('%Y%m%d')
 
     suff = '{}-'.format(suffix) if suffix else ''
-    outfn = '{}{}'.format(suffix, today_date_str)
+    outfn = '{}{}'.format(suff, today_date_str)
     folderpath = os.path.join(root, outfn)
 
     if not os.path.exists(folderpath):
@@ -135,27 +131,30 @@ def from_adc(adc_values, precision=None, sensitivity=None):
     return adc_values * sensitivity / (float(pow(2, precision)) / 2)
 
 def absjoin(paths):
+    """Given a list of paths, join, and return the absolute path."""
     return os.path.abspath(os.path.join(*paths))
 
 def first(iterable):
     """Return first element from a dataset.  Purely for easier reading"""
-    return Nth(iterable, 1)
+    return nth(iterable, 1)
 
 def second(iterable):
     """Return second element from a dataset.  Purely for easier reading"""
-    return Nth(iterable, 2)
+    return nth(iterable, 2)
 
-def Nth(iterable, N=2):
+def nth(iterable, num=2):
     """Return Nth element from a dataset.  Purely for easier reading"""
-    if len(iterable) > (N-1):
-        return iterable[N-1]
+    if len(iterable) > (num-1):
+        return iterable[num-1]
     return []
 
-def NeedsRefactoring(reason):
+def needs_refactoring(reason):
     """Decorate to not allow operation before refactoring."""
     def actual_decorator(func):
+        """Closure around function needing refactoring."""
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper():
+            """Wrapper to raise exception on un-refactored function."""
             # print("{} needs refactoring".format(func.__name__))
             raise Exception('REFACTOR: {}'.format(reason))
         return wrapper
@@ -163,15 +162,15 @@ def NeedsRefactoring(reason):
 
 def time_gen(start, stop, step):
     """Generate timestamps in a range."""
-    def still_in_range(current, end, isBackwards):
-        if isBackwards:
-            return current > end
-        return current < end
-    s, e = dp.parse(start), dp.parse(stop)
-    backwards = True if (s > e) else False
+    now, end = dateutil.parser.parse(start), dateutil.parser.parse(stop)
+    backwards = True if (now > end) else False
     step = -1 * step if backwards else step
-    while still_in_range(s, e, backwards):
-        yield s
-        s += step
-    if str(s) != start:
-        yield s
+    while True:
+        yield now
+        now += step
+        if backwards and now < end:
+            break
+        if not backwards and now > end:
+            break
+    if str(now) != start:
+        yield now
