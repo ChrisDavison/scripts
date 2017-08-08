3ad68aaa9a705bf750e4bdce3624083ea9007d51
Chris Davison <c.jr.davison@gmail.com>
2017-08-08 11:15:57 +0100

    Removing unused functionality from plottools

--- plottools.py
+++ plottools.py
@@ -4,68 +4,40 @@ import re
 import os
 import os.path
 
-dt = datetime
 
 def format_specgram_axis(ax):
+    """Add time ticks to a specgram x-axis"""
     def timeTicks(x, pos):
-        d = dt.timedelta(seconds=x)
+        d = datetime.timedelta(seconds=x)
         return str(d)
     formatter = matplotlib.ticker.FuncFormatter(timeTicks)
     ax.xaxis.set_major_formatter(formatter)
 
-def __fn_sanitise(s):
-    for c in r'[]/\;,><&*:%=+@!#^()|?^':
-        s = s.replace(c,'')
-    return s
-
-def formatted_date():
-    return dt.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
 def dated_title(title=None, ext='png'):
-    fmtdate = formatted_date()
-    title = __fn_sanitise(title).replace(" ", "-").lower()
-    return "{}--{}.{}".format(fmtdate, title, ext)
-
-def dated_fig_dir_and_filename(notebook_name=None, title=None, ext='png'):
-    curdir = os.getcwd()
-    plotdir = os.path.join(curdir, '..', '__PLOTS', __fn_sanitise(notebook_name))
-    plotdir_abs = os.path.abspath(plotdir)
-    out = os.path.join(plotdir_abs, dated_title(title, ext))
-    return plotdir_abs, out
+    """Get time of save in YYYY-MM-DD_hh-mm format, with title and extension."""
+    d = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    if not title:
+        return "{}.{}".format(d, ext)
+    sanitised = re.sub(r'[\[\]/\;,><&*:%=+@!#^()|?^]', '', title)
+    title = sanitised.replace(" ", "-").lower()
+    return "{}_{}.{}".format(d, title, ext)
 
 
-def savefig(figure, *, project=None, trial=None, task=None, title=None, ext='png', **kwargs):
-    def get_title(title, ext):
-        d = formatted_date()
-        if title is None:
-            return "{}.{}".format(d, ext)
-        return "{}_{}.{}".format(d, title, ext)
+def savefig(figure, *, project_and_trial='', task='', title=None, ext='png', **kwargs):
+    """Save a figure into an env-variable defined directory
+
+    Using the env variable RESEARCHFIGURES, save the passed figure into directory
+    `project_and_trial`, with subdirectory `task` and a timestamped title"""
     dir_figures = os.environ['RESEARCHFIGURES']
-    dir_saved = ""
     if dir_figures == '' or not os.path.exists(dir_figures):
         raise Exception('Need environment RESEARCHFIGURES set to a directory')
     if figure is None:
         raise Exception("Nothing to save!  Need to pass a figure.")
-    if project is None:
-        dir_saved = os.path.join(dir_figures)
-    elif trial is None:
-        dir_saved = os.path.join(dir_figures, project)
-    elif task is None:
-        dir_saved = os.path.join(dir_figures, project, trial)
-    else:
-        dir_saved = os.path.join(dir_figures, project, trial, task)
+    dir_saved = os.path.join(dir_figures, project_and_trial, task)
     if not os.path.exists(dir_saved):
         print('dir: ', dir_saved)
         os.makedirs(dir_saved)
-    filename_saved = os.path.join(dir_saved, get_title(title, ext))
+    filename_saved = os.path.join(dir_saved, dated_title(title, ext))
     figure.savefig(filename_saved, **kwargs)
     return filename_saved
-
-def save_dated_fig(fig, *, notebook_name=None, title=None, ext='png'):
-    """Save in ./../__PLOTS a file with date as prefix."""
-    if not notebook_name:
-        raise Exception("Must give a notebook name.")
-    plotdir, fn_out = dated_fig_dir_and_filename(notebook_name, title, ext)
-    if not os.path.exists(plotdir):
-        os.mkdir(plotdir)
-    fig.savefig(fn_out)
