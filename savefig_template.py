75ff4392d6a338f63daca02f37e7213a69e77995
Chris Davison <c.jr.davison@gmail.com>
2017-09-04 15:14:57 +0100

    Removing unused scripts

--- savefig_template.py
+++
@@ -1,9 +0,0 @@
-FIGSAVE_CFG = {'project': 'project'
-               , 'trial': 'trial'
-               , 'task' : 'task'}
-
-# Use `savefig(figure, *, project, trial, task, title=None, ext='png', **kwargs)`
-# or simpler `savefig(figure, *, title=None, ext='png', **FIGSAVE_CFG, **kwargs)`
-# to save a dated figure in a nested directory.
-# -----------------------------------------------------------------------------
-# Uses OS environment variable 'RESEARCHFIGURES' as parent directory
