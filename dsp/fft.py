4a10a59ad89f34c86a0e19c4b747acb24d04ca38
Chris Davison <c.jr.davison@gmail.com>
2017-11-19 10:00:26 +0000

    Normalise FFT by signal length

--- dsp/fft.py
+++ dsp/fft.py
@@ -2,7 +2,7 @@ import scipy.fftpack as sfft
 
 
 def easy_fft(data, fs):
-    X = abs(sfft.fft(data))
+    X = abs(sfft.fft(data)) / len(X)
     f = sfft.fftfreq(len(X), 1/fs)
     N = int(len(X) / 2)
     return X[:N], f[:N]
