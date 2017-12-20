8eeb1a423812e2e6a37093efb1aa4f25b9d22252
Chris Davison <c.jr.davison@gmail.com>
2017-12-20 14:48:32 +0000

    Broken link check only local files

--- blc/blc.js
+++ blc/blc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
 'use strict';
 
@@ -8,8 +8,6 @@ var chalk = require('chalk');
 var fs = require('fs');
 var markdownLinkCheck = require('markdown-link-check');
 var program = require('commander');
-var request = require('request');
-var url = require('url');
 var path = require('path');
 
 var statusLabels = {
@@ -17,51 +15,45 @@ var statusLabels = {
     dead: chalk.red('BAD')
 };
 
+const getArgs = () => {
+    var opts = {};
+    var stream = process.stdin; // read from stdin unless a filename is given
+    program
+        .version('0.1.0')
+        .option('-p, --progress', 'show progress bar')
+        .option('-b, --badonly', 'only show failed links')
+        .option('-v, --verbose', 'Verbose output')
+        .arguments('[filename]').action(filename => {
+            opts.baseUrl = 'file://' + path.dirname(path.resolve(filename));
+            opts.filename = filename;
+            stream = fs.createReadStream(filename);
+        })
+        .parse(process.argv);
+    opts.showProgressBar = (program.progress === true); // force true or undefined to be true or false.
+    opts.badOnly = (program.badonly === true);  // Only show failed links
+    opts.verbose = (program.verbose === true);
+    opts.stream = stream;
+    return opts;
+};
+
+let opts = getArgs();
+
+const display = (filename, status, link, badOnly) => {
+    if(status === 'dead'){
+        console.log('%s: %s', chalk.red(filename), link);
+    } else if (status === 'alive' && !badOnly) {
+        console.log('%s: %s', chalk.green(filename), link);
+    }
+};
+
 var error = false;
-var opts = {};
-var stream = process.stdin; // read from stdin unless a filename is given
-program
-    .version('0.1.0')
-    .option('-p, --progress', 'show progress bar')
-    .option('-b, --badonly', 'only show failed links')
-    .option('-v, --verbose', 'Verbose output')
-    .arguments('[filenameOrUrl]').action(function (filenameOrUrl) {
-        if (/https?:/.test(filenameOrUrl)) {
-            stream = request.get(filenameOrUrl);
-            try { // extract baseUrl from supplied URL
-                var parsed = url.parse(filenameOrUrl);
-                delete parsed.search;
-                delete parsed.hash;
-                if (parsed.pathname.lastIndexOf('/') !== -1) {
-                    parsed.pathname = parsed.pathname.substr(0, parsed.pathname.lastIndexOf('/') + 1);
-                }
-                opts.baseUrl = url.format(parsed);
-            } catch (err) { /* ignore error */ }
-        } else {
-            opts.baseUrl = 'file://' + path.dirname(path.resolve(filenameOrUrl));
-            stream = fs.createReadStream(filenameOrUrl);
-        }})
-    .parse(process.argv);
-
-opts.showProgressBar = (program.progress === true); // force true or undefined to be true or false.
-opts.badOnly = (program.badonly === true);  // Only show failed links
-opts.verbose = (program.verbose === true);
-
 var markdown = ''; // collect the markdown data, then process it
-stream.on('data', (chunk) => {
+opts.stream.on('data', (chunk) => {
     markdown += chunk.toString();
-}).on('end', () => {
-    markdownLinkCheck(markdown, opts, (err, results) => {
-        if(opts.verbose && results.length === 0){
-            console.log(chalk.yellow('No hyperlinks found!'));
-        }
-        results.forEach(result => {
-            if(result.status === 'dead'){
-                console.log('%s: %s', chalk.red('BAD'), result.link);
-                error = true;
-            } else if (result.status === 'alive' && !opts.badOnly){
-                console.log('%s: %s', chalk.green('GG'), result.link);
-            }
-        });
-    });
-});
+}).on('end', () =>     
+    markdownLinkCheck(markdown, opts, (err, results) =>         
+        results.forEach(result => 
+            display(opts.filename, result.status, result.link, opts.badOnly))
+));
+
+
