7dcd91a01ddeea8733adb4d161b20e31685b5513
Christopher Davison <c.jr.davison@gmail.com>
2018-10-05 16:17:43 +0100

    Move git util functons to python

--- dupfinder.py
+++ dupfinder.py
@@ -12,21 +12,23 @@ import hashlib
 from collections import defaultdict
 from docopt import docopt
 
+
 def find_duplicates(parent, verbose=False):
     """Find all duplicates within a parent folder."""
-    dups = defaultdict(list) # {hash: filename}
+    dups = defaultdict(list)  # {hash: filename}
     for directory, _, file_list in os.walk(parent):
         if verbose:
-            print('Scanning {}...'.format(directory))
+            print("Scanning {}...".format(directory))
         for filename in file_list:
-            path = os.path.join(directory, filename) # Get the path to the file
-            if 'git' in path:
+            path = os.path.join(directory, filename)  # Get the path to the file
+            if "git" in path:
                 if verbose:
                     print("Skipping {}".format(path))
                 continue
-            file_hash = hashfile(path) # Calculate hash
+            file_hash = hashfile(path)  # Calculate hash
             dups[file_hash].append(path)
     return dups
+
 
 def join_dicts(dict1, dict2):
     """Join two dictionaries."""
@@ -36,9 +38,10 @@ def join_dicts(dict1, dict2):
         else:
             dict1[key] = dict2[key]
 
+
 def hashfile(path, blocksize=65536):
     """Get the hash digest of a file, using md5."""
-    afile = open(path, 'rb')
+    afile = open(path, "rb")
     hasher = hashlib.md5()
     buf = afile.read(blocksize)
     while buf:
@@ -47,16 +50,17 @@ def hashfile(path, blocksize=65536):
     afile.close()
     return hasher.hexdigest()
 
+
 def show_results(dict1):
     """Show the duplicates found."""
     results = list(filter(lambda x: len(x) > 1, dict1.values()))
     if results:
-        print('Duplicates Found.  These files have identical CONTENTS.')
-        print('_' * 20)
+        print("Duplicates Found.  These files have identical CONTENTS.")
+        print("_" * 20)
         for result in results:
             for subresult in result:
-                print('\t\t%s' % subresult)
-            print('-' * 20)
+                print("\t\t%s" % subresult)
+            print("-" * 20)
 
 
 def __main(folders, verbose):
@@ -69,11 +73,11 @@ def __main(folders, verbose):
         if os.path.exists(folder):
             join_dicts(dups, find_duplicates(folder, verbose))
         else:
-            print('{} is not a valid path, please verify'.format(folder))
+            print("{} is not a valid path, please verify".format(folder))
             sys.exit()
     show_results(dups)
 
 
-if __name__ == '__main__':
-    ARGS = docopt(__doc__, version='0.0.1')
-    __main(ARGS['<folders>'], ARGS['--verbose'])
+if __name__ == "__main__":
+    ARGS = docopt(__doc__, version="0.0.1")
+    __main(ARGS["<folders>"], ARGS["--verbose"])
--- list-dropbox-contents.py
+++ list-dropbox-contents.py
@@ -2,7 +2,7 @@ import sys
 import dropbox
 
 
-__TOKEN = ''
+__TOKEN = ""
 dbx = dropbox.Dropbox(__TOKEN)
 
 
@@ -12,27 +12,27 @@ def get_folders_under_path(path, ignores):
     isFolder = lambda x: isinstance(x, dropbox.files.FolderMetadata)
     for f in filter(isFolder, response.entries):
         p = f.path_lower
-        if len(p.split('/')) < 5 and not any (i.lower() in p for i in ignores):
+        if len(p.split("/")) < 5 and not any(i.lower() in p for i in ignores):
             out.extend(get_folders_under_path(f.path_lower, ignores))
     return out
 
 
 def indented_str(path, max_level):
-    parts = path.split('/')
+    parts = path.split("/")
     indent = len(parts) - 2
     if indent == 0:
-        out = '\n# ' + parts[-1] + '\n\n'
+        out = "\n# " + parts[-1] + "\n\n"
     elif indent <= max_level:
-        out = "- `|{} {}`".format('-' * indent * 2, parts[-1])
+        out = "- `|{} {}`".format("-" * indent * 2, parts[-1])
     else:
         out = ""
     return out
 
 
-if __name__ == '__main__':
-    paths = [p 
-             for p in get_folders_under_path("", ['Apps', 'Camera Uploads'])
-             if p is not '']
-    with open('contents-tree.md', 'w') as f_out:
+if __name__ == "__main__":
+    paths = [
+        p for p in get_folders_under_path("", ["Apps", "Camera Uploads"]) if p is not ""
+    ]
+    with open("contents-tree.md", "w") as f_out:
         for p in sorted(paths):
             print(indented_str(p, max_level=10), file=f_out)
