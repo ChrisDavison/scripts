e7f314ac95d6350d59ea99ff313836f9c9439402
Christopher Davison <c.jr.davison@gmail.com>
2018-10-06 19:30:37 +0100

    Refactor dupfinder for clarity
    Convert to a class, as there are multiple dependant methods with data that can be shared.

--- dupfinder.py
+++ dupfinder.py
@@ -4,42 +4,48 @@ import glob
 import hashlib
 import os
 import sys
+from multiprocessing.pool import Pool
 from collections import defaultdict
-from itertools import chain
+   
 
+class Dupfinder:
+    def __init__(self, folders, blocksize):
+        self.blocksize = blocksize
+        self.files = []
+        for folder in folders:
+            paths = glob.glob(f"{folder}/**/*", recursive=True)
+            self.files.extend(p for p in paths if not os.path.isdir(p))
 
-def find_dups(folders, blocksize):
-    """Find files with duplicate hashes"""
-    files = []
-    for folder in folders:
-        files.extend(glob.glob(f"{folder}/**.*", recursive=True))
-    hashes = defaultdict(lambda: [])
-    for path in set(files):
-        hashes[filehash(path, blocksize)].append(path)
-    hasdups = [ls for _, ls in hashes.items() if len(ls) > 1]
-    if not hasdups:
-        return
-    print("These files have identical content")
-    for paths in hasdups:
-        print('=' * 40)
-        for p in sorted(paths):
-            print(f"\t{p}")
+    def run(self):
+        path_and_hash = Pool().map(self.hash, set(self.files))
+        hashes = defaultdict(lambda: [])
+        for path, hash in path_and_hash:
+            hashes[hash].append(path)
+        self.hasdups = [ls for _, ls in hashes.items() if len(ls) > 1]
+        if not self.hasdups:
+            return
+        self.display()
 
-
-def filehash(path, blocksize):
-    """Get MD5 hash of a file"""
-    hasher = hashlib.md5()
-    with open(path, "rb") as f:
-        buf = f.read(blocksize)
-        while buf:
-            hasher.update(buf)
-            buf = f.read(blocksize)
-    return hasher.hexdigest()
+    def display(self):
+        for paths in self.hasdups:
+            print('=' * 40)
+            for p in sorted(paths):
+                print(f"\t{p}")
+    
+    def hash(self, filename):
+        """Get MD5 hash of a file"""
+        hasher = hashlib.md5()
+        with open(filename, "rb") as f:
+            buf = f.read(self.blocksize)
+            while buf:
+                hasher.update(buf)
+                buf = f.read(self.blocksize)
+        return filename, hasher.hexdigest()
 
 
 if __name__ == "__main__":
     args = list(sys.argv[1:])
     if args:
-        find_dups(set(args), blocksize=65536)
+        Dupfinder(set(args), blocksize=65536).run()
     else:
         print("usage: dupfinder.py <dir>...")
