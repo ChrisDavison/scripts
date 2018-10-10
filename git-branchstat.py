de35b7ab8661c7dd7c1603cd1f56edbe0ce18dde
Christopher Davison <c.jr.davison@gmail.com>
2018-10-10 17:47:09 +0100

    Fix: strip output (empty last line) to get proper count

--- git-branchstat.py
+++ git-branchstat.py
@@ -22,13 +22,13 @@ def get_num_modified_and_untracked():
     """Count number of untracked and modified files in repo"""
     out = subprocess.run(
         ["git", "diff", "--stat"], stdout=subprocess.PIPE
-    ).stdout.decode(encoding="UTF-8")
+    ).stdout.decode(encoding="UTF-8").strip()
     n_modified = len(out.split("\n")) - 1
 
     out = subprocess.run(
         ["git", "ls-files", "--others", "--exclude-standard"], stdout=subprocess.PIPE
-    ).stdout.decode(encoding="UTF-8")
-    n_untracked = len(out.split("\n")) - 1
+    ).stdout.decode(encoding="UTF-8").strip()
+    n_untracked = len(out.split("\n"))
 
     if n_untracked and n_modified:
         print(f"HEAD [modified {n_modified}, untracked {n_untracked}]")
