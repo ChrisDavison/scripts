0889c70c576aca82dc82826681789d88fc480ea0
Christopher Davison <c.jr.davison@gmail.com>
2018-11-07 11:46:46 +0000

    Print a random quote from ~/.quotes

---
+++ randomquote.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os.path
+from pathlib import Path
+import random
+import re
+import sys
+import textwrap
+
+
+FILENAME=os.path.expanduser('~/.quotes')
+p = Path(FILENAME)
+if not p.exists():
+    print("No file: ~/.quotes")
+    sys.exit(1)
+contents = p.read_text().splitlines()
+quotes = [l for l in contents if not l.startswith('---')]
+
+quote = random.choice(quotes)
+
+border = '+%s+\n| %-60s |' % (('-'*62), '')
+
+print(border)
+for line in textwrap.wrap(quote, 60):
+    print('| %-60s |' % line)
+print(border[::-1])
+
