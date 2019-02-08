49868ce8c6edd0a702c324ec985aa7e4df0ac2e5
Chris Davison <c.jr.davison@gmail.com>
2019-02-08 22:51:35 +0000

    Move functionality to dotfiles

--- fetch.py
+++
@@ -1,9 +0,0 @@
-"""Fetch and print URLs"""
-import sys
-import requests
-
-
-for url in sys.argv[1:]:
-    response = requests.get(url)
-    if response.text:
-        print(response.text)
--- fromepoch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-date -r "$1" +"%Y%m%d %H:%M:%S"
