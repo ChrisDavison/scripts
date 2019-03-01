45f22f5cafc8a899cb46ce00581637c0af21ad72
Chris Davison <c.jr.davison@gmail.com>
2019-03-01 11:39:49 +0000

    Convert asmr.csv to pinboard format

---
+++ asmr-to-pinboard-format.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+html="""<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks Menu</H1>
+
+<DL><p>
+{bookmarks}
+</DL>"""
+
+def to_pinboard(string):
+    title, id = string.strip().split(';')
+    if title.startswith('+'):
+        title = title[1:]
+    link="https://www.youtube.com/watch?v={}".format(id)
+    return "<DT><A HREF=\"{link}\">{title}</A>".format(link=link, title=title)
+
+
+def main():
+    filename = os.environ['ASMRFILE']
+    entries = open(filename).read().split('\n')
+    formatted_entries = '\n'.join(to_pinboard(e) for e in entries if e)
+    print(html.format(bookmarks=formatted_entries))
+
+
+if __name__ == "__main__":
+    main()
+
