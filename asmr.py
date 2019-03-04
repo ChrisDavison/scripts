84ca1b0cb4dd0bb5fa1165ab24c33fe5413663cc
Chris Davison <c.jr.davison@gmail.com>
2019-03-04 11:58:35 +0000

    Use functions instead of classes

--- asmr.py
+++ asmr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from enum import Enum
+import argparse
 import json
 import os
 import random
@@ -15,108 +16,105 @@ class Errors(Enum):
 
 
 class style:
-    BOLD = '\033[1m'
-    END = '\033[0m'
+    BOLD = "\033[1m"
+    END = "\033[0m"
+    FG_Black = "\033[30m"
+    FG_Red = "\033[31m"
+    FG_Green = "\033[32m"
+    FG_Yellow = "\033[33m"
+    FG_Blue = "\033[34m"
+    FG_Magenta = "\033[35m"
+    FG_Cyan = "\033[36m"
+    FG_White = "\033[37m"
+    BG_Black = "\033[40m"
+    BG_Red = "\033[41m"
+    BG_Green = "\033[42m"
+    BG_Yellow = "\033[43m"
+    BG_Blue = "\033[44m"
+    BG_Magenta = "\033[45m"
+    BG_Cyan = "\033[46m"
+    BG_White = "\033[47m"
 
 
-class asmr_vids:
-    def __init__(self, filename):
-        self.entries = json.load(open(filename, encoding='utf8'))
-        self.filtered = self.entries
-        self.choice = None
+def display(entries):
+    for i, entry in enumerate(entries):
+        print(f"{i:4} {format_entry(entry)}")
 
 
-    def display(self):
-        for i, entry in enumerate(self.filtered):
-            print(f"{i:4} {self[i]}")
+def add_to(entries, filename):
+    artist = input("Artist: ")
+    title = input("Title: ")
+    fav = input("Fav [y/n]: ").lower()
+    vid = input("Video ID: ")
+    if len(vid) < 11:
+        print("Video hash must be >= 11 characters")
+        raise Errors.short_vid_hash
+    if any(True for e in entries if e["hash"] == vid):
+        raise Errors.vid_already_exists
+    new_vid = {
+        "artist": artist,
+        "title": title,
+        "fav": True if fav[0] == "y" else False,
+        "hash": vid,
+    }
+    entries.append(new_vid)
+    yaml.dump(entries, open(filename, "w", encoding="utf8"), indent=2)
 
 
-    def choose(self):
-        self.display()
-        choice = int(input("Which one?: "))
-        self.choice = self.filtered[choice]
+def filter(query, entries):
+    q = query.lower()
+    matches = [
+        e for e in entries if q in e["title"].lower() or q in e["artist"].lower()
+    ]
+    if not matches:
+        raise Errors.no_vid
+    return matches
 
 
-    def random(self):
-        N = len(self.filtered)
-        self.choice = self.filtered[random.randint(0, N)]
-
-
-    def filter(self, query):
-        q = query.lower()
-        self.filtered = [e for e in self.entries
-                         if q in e['artist'].lower() or q in e['title'].lower()]
-        if not self.filtered:
-            print("No matching videos")
-            sys.exit(Errors.no_vid)
-
-
-    def __getitem__(self, index):
-        e = self.filtered[index]
-        s = style.BOLD if e['fav'] else ''
-        return f"{s}{e['artist']:20}{e['title']}{style.END}"
-
-
-    def __str__(self):
-        if self.choice:
-            s = style.BOLD if self.choice['fav'] else ''
-            return f"{s}{self.choice['artist']:20}{self.choice['title']}{style.END}"
+def choose(entries, get_random_video=False):
+    choice = random.randint(0, len(entries) - 1)
+    if not get_random_video:
+        if len(entries) == 1:
+            choice = 0
         else:
-            return "No matching video."
+            display(entries)
+            choice = int(input("Choice: "))
+    return entries[choice]
 
 
-    def open(self):
-        if self.choice:
-            print(self)
-            url = f"https://www.youtube.com/watch?v={self.choice['hash']}"
-            webbrowser.open(url)
-            sys.exit(Errors.none)
-        else:
-            print("No matching video")
-            sys.exit(Errors.no_vid)
+def format_entry(e):
+    s = f"{style.BOLD}{style.FG_Red}" if e["fav"] else ""
+    return f"{s}{e['artist']:20}{e['title']}{style.END}"
 
 
-    def add(self, artist, title, fav, hashid):
-        any_matching = [i for i, e in enumerate(self.entries)
-                        if hashid == e['hash']]
-        if any_matching:
-            print(f"Video already exists: {self[any_matching[0]]}")
-            sys.exit(Errors.vid_already_exists)
-        else:
-            self.entries.append({'artist': artist, 'title': title, 'fav': fav, 'hash': hashid})
+def open_in_browser(e):
+    url = f"https://www.youtube.com/watch?v={e['hash']}"
+    webbrowser.open(url)
 
 
-def main():
-    filename = os.path.expanduser('~/Dropbox/asmr.json')
-    vids = asmr_vids(filename)
-    flags = [a for a in sys.argv[1:] if a.startswith('-')]
-    args = [a for a in sys.argv[1:] if not a.startswith('-')]
-    add_an_entry = '-a' in flags
-    just_pick_random = '-r' in flags
-    just_list = '-l' in flags
-    if add_an_entry:
-        artist = input("Artist: ")
-        title = input("Title: ")
-        fav = input("Fav [y/n]: ").lower()
-        vid = input("Video ID: ")
-        if len(vid) < 11:
-            print("Video hash must be >= 11 characters")
-            sys.exit(Errors.short_vid_hash)
-        vids.add(artist, title, True if fav == 'y' else False, vid)
-        print(vids[len(vids.entries)-1])
-        json.dump(vids.entries, open(filename, 'w', encoding='utf8'), indent=2)
+try:
+    parser = argparse.ArgumentParser("Open or add an asmr video")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-r", help="Open a random video", action="store_true")
+    group.add_argument("-a", help="Add a video", action="store_true")
+    group.add_argument("-l", help="List videos", action="store_true")
+    parser.add_argument("query", help="Query to filter by", nargs="?", default="")
+    args = parser.parse_args()
+
+    filename = os.path.expanduser("~/Dropbox/asmr.json")
+    vids = json.load(open(filename, encoding="utf8"))
+    vids = sorted(vids, key=lambda x: x["artist"])
+    vids = filter(args.query, vids)
+
+    if args.a:
+        add_to(vids, filename)
+    elif args.l:
+        display(vids)
     else:
-        query = ' '.join(args)
-        if query:
-            vids.filter(query)
-        if just_pick_random:
-            vids.random()
-        if not just_pick_random:
-            vids.choose()
-        vids.open()
-
-
-if __name__ == "__main__":
-    main()
-
-
+        choice = choose(vids, args.r == True)
+        print(format_entry(choice))
+        open_in_browser(choice)
+except (EOFError, KeyboardInterrupt):
+    print("\nNo video selected. Exiting...")
+except Exception as E:
+    print(E)
