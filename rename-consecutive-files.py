e890bf3df8cb102c79c997a738e7d71b7661b587
Chris Davison <c.jr.davison@gmail.com>
2019-03-18 10:08:04 +0000

    Add some whitespaceto consec-file renamer

--- rename-consecutive-files.py
+++ rename-consecutive-files.py
@@ -5,7 +5,9 @@ from shutil import move
 if len(sys.argv) < 2:
     print("usage: rename-consecutive-files.py <DIR>"
     sys.exit(1)
+
 dir = sys.argv[1]
+
 for i, p in enumerate(Path(dir).glob('*')):
     new = p.parent / f"{i:02d}{p.suffix}"
     move(p, new)
