afbbcd69a9ae18fa2b85cd6ba25d0ea7b527dab6
Chris Davison <c.jr.davison@gmail.com>
2019-03-25 12:04:31 +0000

    WIP todo dataclass

--- t.py
+++ t.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 import os
 import re
+from dataclasses import dataclass
+from typing import Optional, List, Any
 
-def parse_todo(line):
+@dataclass
+class Todo:
+    due: Optional[str]
+    task: str
+
+    def __repr__(self):
+        d = self.due if self.due else ''
+        return f"{d:10s} {self.task}"
+
+
+def parse_todo(line: str) -> Todo:
     line = line.strip()
     if line.startswith("- "):
         line = line[2:]
     date, task = None, line
     if re.match("\d\d\d\d-\d\d-\d\d", line):
         date, task = line[:10], line[11:]
-    return {'task': task, 'date': date, 'done': False}
+    return Todo(date, task)
 
-def printable(task):
-    d = task['date'] if task['date'] else ''
-    return f"{task['idx']:3d}. {d:10s} {task['task']}"
 
-def writeable(task):
-    d = task['date'] if task['date'] else ''
-    return f"- {task['idx']:3d}. {d:10s} {task['task']}"
+def add(text: str) -> Todo:
+    m = re.match(r"(\d{4}-\d{2}-\d{2}) (.*)", text)
+    due, task = (m.group(1), m.group(2)) if m else (None, text)
+    t = Todo(due, task)
+    return t
 
-def parse_file(filename, parser):
-    return [{**parser(line), 'idx': i}
-            for i, line
-            in enumerate(open(filename, 'r'))]
+todos = [parse_todo(line) for line in open(os.environ["TODOFILE"], 'r')]
+text = "2019-01-01 This is a task"
+todos.append(add(text))
 
-for line in parse_file(os.environ["TODOFILE"], parse_todo):
-    print(printable(line))
+for t in todos:
+    print(t)
