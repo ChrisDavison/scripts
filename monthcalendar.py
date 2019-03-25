02b531730359bd04b7a8c408811e4bffeca173a2
Chris Davison <c.jr.davison@gmail.com>
2019-03-25 22:50:11 +0000

    Add ability to offset start month to monthcalendar

--- monthcalendar.py
+++ monthcalendar.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """Print out the list of dates in this month, as a markdown list"""
 import datetime
+import sys
 
 
+offset = 0
+if len(sys.argv) > 1:
+    offset = int(sys.argv[1])
 today = datetime.date.today()
-start_of_month = today - datetime.timedelta(days=today.day-1)
-start_month_no = start_of_month.month
-tmp = start_of_month
-while tmp.month == start_month_no:
+tmp = datetime.date(today.year, today.month + offset, 1)
+end = datetime.date(today.year, today.month+1 + offset, 1)
+print(f"# {tmp.strftime('%B')}\n")
+while tmp < end:
     print(f"- `{tmp.day:2d}`: ")
     tmp += datetime.timedelta(days=1)
