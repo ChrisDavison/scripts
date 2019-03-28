bdf2d46f734cc1e1e4549911ebb8e2efb024df79
Chris Davison <c.jr.davison@gmail.com>
2019-03-28 15:25:04 +0000

    Only use simple output for cal.py

--- cal.py
+++ cal.py
@@ -6,52 +6,42 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 
-def month_cal(year, month, start_day_of_week=0, simple_display=False):
-    start_day_of_week = start_day_of_week % 7
+def month_cal(year, month, start_day_of_week=0):
+    start_day_of_week = int(start_day_of_week) % 7
     today = datetime.date.today()
     year = int(year) if year else today.year
     month = int(month) if month else today.month
     start = datetime.date(year, month, 1)
     by_weekday, week, temp = [], [], start
     while temp.month == start.month:
-        if temp.weekday() == start_day_of_week:
-            if week:
-                by_weekday.append(week)
-            week = [temp]
-        else:
-            week.append(temp)
+        if temp.weekday() == start_day_of_week and week:
+            by_weekday.append(week)
+            week = []
+        week.append(temp)
         temp += datetime.timedelta(days=1)
-    by_weekday.append(week)
-    by_weekday
+    if week:
+        by_weekday.append(week)
 
     print(start.strftime("%B %Y").center(20))
     headers = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
     headerstr = ' '.join(headers[start_day_of_week:] + headers[:start_day_of_week])
     N = len(headerstr)
     topbot_border = f'+{"-"*(N+2)}+'
-    pipeize = lambda x: f"| {x} |"
-    if simple_display:
-        pipeize = lambda x: x
-        print(headerstr)
-    else:
-        print(topbot_border)
-        print(pipeize(headerstr))
-        print(topbot_border)
+    outstr = headerstr + "\n"
     for i, week in enumerate(by_weekday):
         aligned_week = [str(w.day).rjust(2) for w in week] 
         w = ' '.join(aligned_week)
         justified = w.ljust(20) if i else w.rjust(20)
-        print(pipeize(justified))
-    if not simple_display:
-        print(topbot_border)
+        outstr += justified + "\n"
+    return outstr
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument('-y', '--year')
     p.add_argument('-m', '--month')
-    p.add_argument('-s', '--simple', help="Print without border",
-            action="store_true")
+    p.add_argument('-s', '--startofweek', default=0,
+            help='Day of week to start (0=Monday)')
     args = p.parse_args()
-    start_day_of_week = 0
-    sys.exit(month_cal(args.year, args.month, start_day_of_week, args.simple))
+    print(month_cal(args.year, args.month, args.startofweek))
+
