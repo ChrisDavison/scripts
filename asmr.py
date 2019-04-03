3ba6931482d4441045fd19862035dd6235132314
Chris Davison <c.jr.davison@gmail.com>
2019-04-03 20:41:02 +0100

    Migrate asmr to click

--- asmr.py
+++ asmr.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
-"""ASMR video management.
-
-Optionally filtered by QUERY (matching title or artist).
-Looks for 'asmr.json' inside directory defined by $DATADIR, with title, artist, 
-hash, and fav keys.
-
-This script exists because youtube's playlists are pretty balls, and I wanted an
-easier way of jumping to videos based on keyword matches (e.g. if I want to
-watch a tapping video).
-
-usage:
-    asmr add
-    asmr view [-f] [-a] [QUERY...]
-    asmr open [-r|-f] [QUERY...]
-    asmr modify QUERY...
-
-options:
-    -f          Filter to favourites only [default: False]
-    -r          Open a random video [default: False]
-    -a          Show all, including archived videos [default: False]
-"""
+"""ASMR video management"""
 import json
 import os
 import random
@@ -29,7 +9,6 @@ from dataclasses import dataclass, asdict
 from pathlib import Path
 from typing import List, Optional, Any
 
-from docopt import docopt
 import click
 
 
@@ -41,49 +20,75 @@ FILENAME = str((Path(direc) / "asmr.json").resolve())
 
 @click.group()
 def cli():
-    """ASMR video management.
-
-    Optionally filtered by QUERY (matching title or artist).
-    Looks for 'asmr.json' inside directory defined by $DATADIR, with title, artist, 
-    hash, and fav keys.
-
-    This script exists because youtube's playlists are pretty balls, and I wanted an
-    easier way of jumping to videos based on keyword matches (e.g. if I want to
-    watch a tapping video)."""
+    """Handle asmr video data in $DATADIR/asmr.json.  Can view, add, play, or modify
+    metadata.  Exists because youtube's playlists are pretty crap."""
     pass
 
 
 @cli.command(short_help="Add a new video")
 def add():
-    pass
+    """Give the user inputs to add a new video to the ASMRFILE"""
+    vids = load_asmr_file()
+    vids.append(Video.new())
+    write_asmr_file(vids)
 
 
 @cli.command(short_help="View list of videos")
 @click.argument('query', nargs=-1)
-@click.option('-f', '--with-favourites', is_flag=True, default=False)
+@click.option('-f', '--only-favourites', is_flag=True, default=False)
 @click.option('-a', '--with-archived', is_flag=True, default=False)
-def view(query, with_favourites, with_archived):
-    filtered = load_from_json()
-    if with_favourites:
-        filtered = [video for video in filtered if video.fav]
-    if not with_archived:
-        filtered = [video for video in filtered if not video.archived]
+def view(query, only_favourites, with_archived):
+    """List videos, optionally filtered by query or favourites only"""
+    filtered = filter_videos(' '.join(query), only_favourites, with_archived)
     display(filtered)
 
 
 @cli.command(short_help="Modify a video")
 @click.argument('query', nargs=-1)
 def modify(query):
-    pass
+    """Modify an existing video's metadata"""
+    vids = load_asmr_file()
+    query = ' '.join(query)
+    for i, video in enumerate(vids):
+        if query in video:
+            print(f"{i:4} {video}")
+    vid_choice = int(input("Choose: "))
+    print("\n", vids[vid_choice].__repr__(), "\n")
+    options = ["artist", "title", "fav", "archived", "exit"]
+    while True:
+        response = input(", ".join(options) + ": ").lower()
+        if response == "exit":
+            break
+        elif response not in options:
+            break
+        elif response == "artist":
+            vids[vid_choice].artist = input("Artist: ")
+        elif response == "title":
+            vids[vid_choice].title = input("Title: ")
+        elif response == "fav":
+            vids[vid_choice].fav = not vids[vid_choice].fav
+            print(f"'Favourite' toggled to {vids[vid_choice].fav}")
+        elif response == "archived":
+            vids[vid_choice].archived = not vids[vid_choice].archived
+            print(f"'Archived' toggled to {vids[vid_choice].archived}")
+        else:
+            raise Exception("Shouldn't be possible to get here...'%s'" % response)
+        print()
+    print("\n", vids[vid_choice].__repr__())
+    write_asmr_file(vids)
 
 
 @cli.command(short_help="Play a video")
 @click.argument('query', nargs=-1)
 @click.option('-r', '--random', is_flag=True, default=False)
-def play(query, random):
-    print(' '.join(query), random)
-
-
+@click.option('-f', '--only-favourites', is_flag=True, default=False)
+@click.option('-a', '--with-archived', is_flag=True, default=False)
+def play(query, random, only_favourites, with_archived):
+    """Play a video (optionally filtered)"""
+    filtered = filter_videos(' '.join(query), only_favourites, with_archived)
+    choice = choose(filtered, random) 
+    print(choice)
+    choice.open()
 
 
 @dataclass
@@ -95,7 +100,7 @@ class Video:
 
     title: str
     artist: str
-    vid: str
+    vid_id: str
     fav: bool
     archived: bool
 
@@ -114,7 +119,7 @@ class Video:
         return f"[{fav}|{arc}] {self.title} by {self.artist}"
 
     def open(self):
-        url = f"https://www.youtube.com/watch?v={self.vid}"
+        url = f"https://www.youtube.com/watch?v={self.vid_id}"
         webbrowser.open(url)
 
     @staticmethod
@@ -141,6 +146,15 @@ class Video:
         return Video(title, artist, vid, fav, False)
 
 
+def filter_videos(query, only_favourites, with_archived):
+    filtered = [vid for vid in load_asmr_file() if query.lower() in vid]
+    if only_favourites:
+        filtered = [video for video in filtered if video.fav]
+    if not with_archived:
+        filtered = [video for video in filtered if not video.archived]
+    return filtered
+
+
 def display(entries: List[Video]):
     """For every asmr entry, display it's index and a pretty printed title"""
     title = f"   # F A {'ARTIST'.ljust(20)}TITLE"
@@ -150,104 +164,39 @@ def display(entries: List[Video]):
         print(f"{i:4} {entry}")
     
 
-def load_from_json() -> List[Video]:
-    vids = json.load(open(FILENAME, encoding="utf8"))
-
-    def json_to_Video(j):
-        return Video(j["title"], j["artist"], j["hash"], j["fav"], j["archived"])
-
-    return sorted([json_to_Video(j) for j in vids], key=lambda x: x.artist)
-
-
-# def write_vids(videos: List[Video]):
-#     if not videos:
-#         raise Exception("Sent empty list of videos to write to file")
-#     def replace_vid_to_hash(video):
-#         h = video["vid"]
-#         del video["vid"]
-#         return {"hash": h, **video}
-
-#     entries = [replace_vid_to_hash(asdict(video)) for video in videos]
-#     if not entries:
-#         raise Exception("Entries in write_vids is empty")
-#     json.dump(entries, open(FILENAME, "w", encoding="utf8"), indent=2)
+def load_asmr_file() -> List[Video]:
+    """Read a json file and convert entries to Video."""
+    videos = []
+    for entry in json.load(open(FILENAME, encoding="utf8")):
+        videos.append(Video(
+            entry["title"],
+            entry["artist"],
+            entry["vid_id"],
+            entry["fav"],
+            entry["archived"]))
+    return sorted(videos, key=lambda x: x.artist)
 
 
-# def modify(vids, query):
-#     for i, video in enumerate(vids):
-#         if query in video:
-#             print(i, video)
-#     vid_choice = int(input("Choose: "))
-#     print(vids[vid_choice].__repr__())
-#     print()
-#     options = ["artist", "title", "fav", "archived", "exit"]
-#     while True:
-#         response = input(", ".join(options) + ": ").lower()
-#         if response == "exit":
-#             break
-#         elif response not in options:
-#             break
-#         elif response == "artist":
-#             vids[vid_choice].artist = input("Artist: ")
-#         elif response == "title":
-#             vids[vid_choice].title = input("Title: ")
-#         elif response == "fav":
-#             vids[vid_choice].fav = not vids[vid_choice].fav
-#             print(f"'Favourite' toggled to {vids[vid_choice].fav}")
-#         elif response == "archived":
-#             vids[vid_choice].archived = not vids[vid_choice].archived
-#             print(f"'Archived' toggled to {vids[vid_choice].archived}")
-#         else:
-#             raise Exception("Shouldn't be possible to get here...'%s'" % response)
-#     print(vids[vid_choice].__repr__())
-#     print()
-#     happy = input("Happy? [y/n] ").lower()[0] == "y"
-#     if not happy:
-#         raise Exception("Not happy with result. Re-run if needed")
-#     write_vids(vids)
+def write_asmr_file(videos: List[Video]):
+    """Convert videos to a dict and then export to json file."""
+    if not videos:
+        raise Exception("Sent empty list of videos to write to file")
+    json.dump([asdict(v) for v in videos], open(FILENAME, "w", encoding="utf8"), indent=2)
 
 
-# def choose(list, random=False):
-#     if random:
-#         return list[random.randint(0, len(list) - 1)]
-#     if not list:
-#         raise Exception("List is empty")
-#     if len(list) == 1:
-#         return list[0]
-#     display(list)
-#     choice = int(input("Choose: "))
-#     return list[choice]
-        
+def choose(list, random=False):
+    """Choose an entry from a list, or get a random one."""
+    if random:
+        return random.choice(list)
+    if not list:
+        raise Exception("List is empty")
+    if len(list) == 1:
+        return list[0]
+    display(list)
+    choice = int(input("Choose: "))
+    return list[choice]
 
-# def main():
-#     """Run asmr video listing or choice"""
-#     args = docopt(__doc__)
-#     vids = load_from_json()
-#     query = " ".join(args["QUERY"])
-#     filtered = [video for video in vids if query in video]
-#     if args["-f"]:
-#         filtered = [video for video in filtered if video.fav]
-#     if not args["-a"]:
-#         filtered = [video for video in filtered if not video.archived]
-
-
-#     try:
-#         if args["add"]:
-#             old_vids = vids.copy()
-#             vids.append(Video.new())
-#             write_vids(vids)
-#         elif args["view"]:
-#             display(filtered)
-#         elif args["modify"]:
-#             modify(vids, query)
-#         elif args["open"]:
-#             choice = choose(filtered, args["-r"]) 
-#             print(choice)
-#             choice.open()
-#         else:
-#             print("Unknown option...shouldn't be able to get here")
-
-# try:
-cli()
-# except Exception as E:
-    # print(E)
+try:
+    cli()
+except Exception as E:
+    print(E)
