d1792f4314c73daef90d5ba71ce1f2eb39606420
Chris Davison <c.jr.davison@gmail.com>
2019-04-05 14:26:20 +0100

    Modify asmr to use enumerated list

--- asmr.py
+++ asmr.py
@@ -12,10 +12,6 @@ from typing import List, Optional, Any
 import click
 
 
-direc = os.environ.get("DATADIR", None)
-if not direc:
-    raise Exception("DATADIR not defined")
-FILENAME = str((Path(direc) / "asmr.json").resolve())
 
 
 @click.group()
@@ -28,9 +24,8 @@ def cli():
 @cli.command(short_help="Add a new video")
 def add():
     """Give the user inputs to add a new video to the ASMRFILE"""
-    vids = load_asmr_file()
-    vids.append(Video.new())
-    write_asmr_file(vids)
+    videos.append((len(videos), Video.new()))
+    write_asmr_file(videos)
 
 
 @cli.command(short_help="View list of videos")
@@ -47,13 +42,12 @@ def view(query, only_favourites, with_archived):
 @click.argument('query', nargs=-1)
 def modify(query):
     """Modify an existing video's metadata"""
-    vids = load_asmr_file()
     query = ' '.join(query)
-    for i, video in enumerate(vids):
-        if query in video:
-            print(f"{i:4} {video}")
-    vid_choice = int(input("Choose: "))
-    print("\n", vids[vid_choice].__repr__(), "\n")
+    options = [(i, video) for i, video in videos if query in video]
+    for idx, (i, video) in enumerate(options):
+        print(f"{idx:4} {video}")
+    vid_choice = options[int(input("Choose: "))][0]
+    print("\n", videos[vid_choice][1].__repr__(), "\n")
     options = ["artist", "title", "fav", "archived", "exit"]
     while True:
         response = input(", ".join(options) + ": ").lower()
@@ -62,20 +56,20 @@ def modify(query):
         elif response not in options:
             break
         elif response == "artist":
-            vids[vid_choice].artist = input("Artist: ")
+            videos[vid_choice][1].artist = input("Artist: ")
         elif response == "title":
-            vids[vid_choice].title = input("Title: ")
+            videos[vid_choice][1].title = input("Title: ")
         elif response == "fav":
-            vids[vid_choice].fav = not vids[vid_choice].fav
-            print(f"'Favourite' toggled to {vids[vid_choice].fav}")
+            videos[vid_choice][1].fav = not videos[vid_choice][1].fav
+            print(f"'Favourite' toggled to {videos[vid_choice][1].fav}")
         elif response == "archived":
-            vids[vid_choice].archived = not vids[vid_choice].archived
-            print(f"'Archived' toggled to {vids[vid_choice].archived}")
+            videos[vid_choice][1].archived = not videos[vid_choice][1].archived
+            print(f"'Archived' toggled to {videos[vid_choice][1].archived}")
         else:
             raise Exception("Shouldn't be possible to get here...'%s'" % response)
         print()
-    print("\n", vids[vid_choice].__repr__())
-    write_asmr_file(vids)
+    print("\n", videos[vid_choice][1].__repr__())
+    write_asmr_file(videos)
 
 
 @cli.command(short_help="Play a video")
@@ -147,11 +141,21 @@ class Video:
 
 
 def filter_videos(query, only_favourites, with_archived):
-    filtered = [vid for vid in load_asmr_file() if query.lower() in vid]
-    if only_favourites:
-        filtered = [video for video in filtered if video.fav]
-    if not with_archived:
-        filtered = [video for video in filtered if not video.archived]
+    filtered = []
+    for i, vid in videos:
+        has_query = query.lower() in vid
+        has_fav = vid.fav or not only_favourites
+        has_archive = not vid.archived or with_archived
+        if has_query and has_fav and has_archive:
+            filtered.append((i, vid))
+
+        # if query.lower() in vid:
+        #     has_query
+    # filtered = [(i, vid) for (i, vid) in videos if query.lower() in vid]
+    # if only_favourites:
+        # filtered = [video for video in filtered if video.fav]
+    # if not with_archived:
+        # filtered = [video for video in filtered if not video.archived]
     return filtered
 
 
@@ -160,8 +164,8 @@ def display(entries: List[Video]):
     title = f"   # F A {'ARTIST'.ljust(20)}TITLE"
     print(title)
     print("-"*(len(title)+20))
-    for i, entry in enumerate(entries):
-        print(f"{i:4} {entry}")
+    for idx, (i, entry) in enumerate(entries):
+        print(f"{idx:4} {entry}")
     
 
 def load_asmr_file() -> List[Video]:
@@ -174,29 +178,35 @@ def load_asmr_file() -> List[Video]:
             entry["vid_id"],
             entry["fav"],
             entry["archived"]))
-    return sorted(videos, key=lambda x: x.artist)
+    return list(enumerate(sorted(videos, key=lambda x: x.artist)))
 
 
-def write_asmr_file(videos: List[Video]):
+def write_asmr_file(videos):
     """Convert videos to a dict and then export to json file."""
     if not videos:
         raise Exception("Sent empty list of videos to write to file")
-    json.dump([asdict(v) for v in videos], open(FILENAME, "w", encoding="utf8"), indent=2)
+    json.dump([asdict(v) for i, v in videos], open(FILENAME, "w", encoding="utf8"), indent=2)
 
 
 def choose(list, random=False):
     """Choose an entry from a list, or get a random one."""
     if random:
-        return random.choice(list)
+        return random.choice(list)[1]
     if not list:
         raise Exception("List is empty")
     if len(list) == 1:
-        return list[0]
+        return list[0][1]
     display(list)
     choice = int(input("Choose: "))
-    return list[choice]
+    return list[choice][1]
+
 
 try:
+    direc = os.environ.get("DATADIR", None)
+    if not direc:
+        raise Exception("DATADIR not defined")
+    FILENAME = str((Path(direc) / "asmr.json").resolve())
+    videos = load_asmr_file()
     cli()
 except Exception as E:
     print(E)
