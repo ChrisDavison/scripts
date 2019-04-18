2112d3b09b8783ce264d18ec8a1a65ed99cdcf4c
Chris Davison <c.jr.davison@gmail.com>
2019-04-18 15:12:34 +0100

    Modify chesstc to use click

--- chesstc.py
+++ chesstc.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
-import os, sys
+import click
 
-args = sys.argv[1:]
-if len(args) < 2:
-    print("usage: chesstc <BASE> <INCREMENT>")
-    sys.exit(1)
 
-base, inc, *rest = args
+@click.command("timecontrol")
+@click.argument("base", nargs=1, type=float)
+@click.argument("increment", nargs=1, type=float)
+def timecontrol(base, increment):
+    """Calculate expected chess game time.
 
-totalsec = float(base) * 60 + float(inc) * 40
-minutes = int(totalsec / 60)
-remsec = int(totalsec - minutes * 60)
-print(f"{minutes}:{remsec}")
+    Assumes 40 moves, using a given base and increment.  Each player will have
+    half of this duration each."""
+    totalsec = (float(base) * 60 + float(increment) * 40) * 2
+    minutes = int(totalsec / 60)
+    remsec = int(totalsec - minutes * 60)
+    print(f"{minutes}:{remsec}")
+
+if __name__ == "__main__":
+    timecontrol()
