736b5f83fd44dcd6306b471fdef9e936f78a0ab3
Chris Davison <c.jr.davison@gmail.com>
2019-04-21 11:38:25 +0100

    Convert asmr to use sqlite

--- asmr.py
+++ asmr.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 """ASMR video management"""
-import json
 import os
-import random
 import re
-import sys
+import sqlite3
 import webbrowser
-from dataclasses import dataclass, asdict
+from random import choice as random_choice
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional, Any
 
 import click
 
 
+DB_FILENAME = 'e:/Dropbox/data/data.db'
+
+
+def execute_select(query):
+    """Execute an sqlite select query, returning column names and data"""
+    db = sqlite3.connect(DB_FILENAME)
+    cursor = db.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    columns = list(map(lambda x: x[0], cursor.description))
+    db.close()
+    return columns, data
 
 
 @click.group()
@@ -25,8 +35,75 @@ def cli():
 @cli.command(short_help="Add a new video")
 def add():
     """Give the user inputs to add a new video to the ASMRFILE"""
-    videos.append((len(videos), Video.new()))
-    write_asmr_file(videos)
+    artist = input("Artist: ")
+    title = input("Title: ")
+    vid = input("Video ID: ")
+    fav = 1 if input("Fav [y/n]: ")[0] in ["Y", "y"] else 0
+
+    if vid.startswith("www.") or vid.startswith("http"):
+        match = re.search(".*?video=(.{11}).*", vid)
+        if match:
+            vid = match.group(1)
+    if not vid:
+        raise Exception("VidAddException: Could not parse video id")
+
+    db = sqlite3.connect(DB_FILENAME)
+    cursor = db.cursor()
+    cursor.execute(f"""
+        INSERT INTO asmr(title, artist, hash, fav, archived)
+        VALUES ( '{title}', '{artist}', '{vid}', '{fav}', '{0}' )
+    """)
+    db.commit()
+    db.close()
+
+
+@cli.command(short_help="Modify a video")
+@click.argument('query', nargs=-1)
+def modify(query):
+    """Modify an existing video's metadata"""
+    query = ' '.join(query).lower()
+    sql_query = f"""
+        select * from asmr
+        where title like '%{query}%' or artist like '%{query}%'
+    """
+    _, data = execute_select(sql_query)
+    videos = [(idx, Video(title, artist, vid_id, fav, archived))
+              for (idx, title, artist, vid_id, fav, archived) in data]
+    for idx, (_, video) in enumerate(videos):
+        print(f"{idx:4} {video}")
+
+    db_id, video = videos[int(input("Choose: "))]
+    print(video)
+    print("Enter new values, or leave blank to keep current")
+    updates = []
+    inp = input(f"artist ({video.artist}): ")
+    if inp:
+        updates.append(f"artist='{inp}'")
+    inp = input(f"title ({video.title}): ")
+    if inp:
+        updates.append(f"title='{inp}'")
+    inp = input(f"hash ({video.vid_id}): ")
+    if inp:
+        updates.append(f"hash='{inp}'")
+    inp = input(f"fav ({video.fav}) (1/0): ")
+    if inp:
+        updates.append(f"fav={int(inp)}")
+    inp = input(f"archived ({video.archived}) (1/0): ")
+    if inp:
+        updates.append(f"archived={int(inp)}")
+
+    if not updates:
+        return
+
+    db = sqlite3.connect(DB_FILENAME)
+    cursor = db.cursor()
+    command = f"""
+        UPDATE asmr
+        SET {','.join(updates)}
+        WHERE id={db_id}
+    """
+    cursor.execute(command)
+    db.commit()
 
 
 @cli.command(short_help="View list of videos")
@@ -35,42 +112,18 @@ def add():
 @click.option('-a', '--with-archived', is_flag=True, default=False)
 def view(query, only_favourites, with_archived):
     """List videos, optionally filtered by query or favourites only"""
-    filtered = filter_videos(' '.join(query), only_favourites, with_archived)
-    display(filtered)
-
-
-@cli.command(short_help="Modify a video")
-@click.argument('query', nargs=-1)
-def modify(query):
-    """Modify an existing video's metadata"""
-    query = ' '.join(query)
-    options = [(i, video) for i, video in videos if query in video]
-    for idx, (i, video) in enumerate(options):
-        print(f"{idx:4} {video}")
-    vid_choice = options[int(input("Choose: "))][0]
-    print("\n", videos[vid_choice][1].__str__(), "\n")
-    options = ["artist", "title", "fav", "archived", "exit"]
-    while True:
-        response = input(", ".join(options) + ": ").lower()
-        if response == "exit":
-            break
-        elif response not in options:
-            break
-        elif response == "artist":
-            videos[vid_choice][1].artist = input("Artist: ")
-        elif response == "title":
-            videos[vid_choice][1].title = input("Title: ")
-        elif response == "fav":
-            videos[vid_choice][1].fav = not videos[vid_choice][1].fav
-            print(f"'Favourite' toggled to {videos[vid_choice][1].fav}")
-        elif response == "archived":
-            videos[vid_choice][1].archived = not videos[vid_choice][1].archived
-            print(f"'Archived' toggled to {videos[vid_choice][1].archived}")
-        else:
-            raise Exception("Shouldn't be possible to get here...'%s'" % response)
-        print()
-    print("\n", videos[vid_choice][1].__str__())
-    write_asmr_file(videos)
+    query = ' '.join(query).lower()
+    fav = "AND fav = 1" if only_favourites else ""
+    archived = "AND archived = 0" if not with_archived else ""
+    sql_query = f"""
+        select * from asmr
+        where (title like '%{query}%' or artist like '%{query}%')
+            {fav} {archived}
+    """
+    _, data = execute_select(sql_query)
+    videos = [(idx, Video(title, artist, vid_id, fav, archived))
+              for (idx, title, artist, vid_id, fav, archived) in data]
+    display(videos)
 
 
 @cli.command(short_help="Play a video")
@@ -80,8 +133,18 @@ def modify(query):
 @click.option('-a', '--with-archived', is_flag=True, default=False)
 def play(query, random, only_favourites, with_archived):
     """Play a video (optionally filtered)"""
-    filtered = filter_videos(' '.join(query), only_favourites, with_archived)
-    choice = choose(filtered, random) 
+    query = ' '.join(query).lower()
+    fav = "AND fav = 1" if only_favourites else ""
+    archived = "AND archived = 0" if not with_archived else ""
+    sql_query = f"""
+        select * from asmr
+        where (title like '%{query}%' or artist like '%{query}%')
+            {fav} {archived}
+    """
+    _, data = execute_select(sql_query)
+    videos = [(idx, Video(title, artist, vid_id, fav, archived))
+              for (idx, title, artist, vid_id, fav, archived) in data]
+    choice = choose(videos, random)
     print(choice)
     choice.open()
 
@@ -92,7 +155,6 @@ class Video:
 
     Contains title, artist, ID (11-character), and indication of favourite
     """
-
     title: str
     artist: str
     vid_id: str
@@ -111,69 +173,18 @@ class Video:
         return f"{fav} {arc} {self.artist:20s}{self.title}"
 
     def open(self):
+        """Generate URL from video id and open in browser"""
         url = f"https://www.youtube.com/watch?v={self.vid_id}"
         webbrowser.open(url)
 
-    @staticmethod
-    def new():
-        """Give the user prompts to create a new asmr video.
-        
-        Always assumes a new video is not archived."""
-        artist = input("Artist: ")
-        title = input("Title: ")
-        vid = input("Video ID: ")
-        fav = input("Fav [y/n]: ")[0] in ["Y", "y"]
 
-        if vid.startswith("www.") or vid.startswith("http"):
-            match = re.search(".*?video=(.{11}).*", url)
-            if match:
-                vid = match.group(1)
-        if not vid:
-            raise Exception("VidAddException: Could not parse video id")
-        return Video(title, artist, vid, fav, False)
-
-
-def filter_videos(query, only_favourites, with_archived):
-    """Get videos, optionall focusing on favourites, or including archived."""
-    filtered = []
-    for i, vid in videos:
-        has_query = query.lower() in vid
-        has_fav = vid.fav or not only_favourites
-        has_archive = not vid.archived or with_archived
-        if has_query and has_fav and has_archive:
-            filtered.append((i, vid))
-    return filtered
-
-
-def display(entries: List[Video]):
+def display(entries):
     """For every asmr entry, display it's index and a pretty printed title"""
     title = f"   # F A {'ARTIST'.ljust(20)}TITLE"
     print(title)
     print("-"*(len(title)+20))
-    for idx, (i, entry) in enumerate(entries):
+    for idx, (_, entry) in enumerate(entries):
         print(f"{idx:4} {entry}")
-    
-
-def load_asmr_file() -> List[Video]:
-    """Read a json file and convert entries to Video."""
-    videos = []
-    for entry in json.load(open(FILENAME, encoding="utf8")):
-        videos.append(Video(
-            entry["title"],
-            entry["artist"],
-            entry["vid_id"],
-            entry["fav"],
-            entry["archived"]))
-    return list(enumerate(sorted(videos, key=lambda x: x.artist)))
-
-
-def write_asmr_file(videos):
-    """Convert videos to a dict and then export to json file."""
-    if not videos:
-        raise Exception("Sent empty list of videos to write to file")
-    videos_without_index = map(lambda x: x[1], videos)
-    videos_as_dicts = map(asdict, videos_without_index)
-    json.dump(list(videos_as_dicts), open(FILENAME, "w", encoding="utf8"), indent=2)
 
 
 def choose(list, random=False):
@@ -181,7 +192,7 @@ def choose(list, random=False):
     if not list:
         raise Exception("List is empty")
     if random:
-        return random.choice(list)[1]
+        return random_choice(list)[1]
     if len(list) == 1:
         return list[0][1]
     display(list)
@@ -194,7 +205,6 @@ try:
     if not direc:
         raise Exception("DATADIR not defined")
     FILENAME = str((Path(direc) / "asmr.json").resolve())
-    videos = load_asmr_file()
     cli()
 except Exception as E:
     print(E)
