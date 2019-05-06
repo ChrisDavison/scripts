92a8b109b3c6f077abd32b344e57d9e241ff65bd
Chris Davison <c.jr.davison@gmail.com>
2019-05-06 12:31:50 +0100

    Use pandas to easily plot tabled bignotes

--- bignotes.py
+++ bignotes.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
-import sys
 from argparse import ArgumentParser
 from pathlib import Path
 from functools import partial
+
+import pandas as pd
 from toolz.functoolz import *
 
 
+def get_parents(path):
+    return '/'.join([str(s) for s in path.parents][::-1][1:])
+
+
+def get_kb(path):
+    return int(path.stat().st_size / 1024)
+
+
 def main():
+    """Find largest markdown files under current directory"""
     parser = ArgumentParser("bignotes")
     parser.add_argument("-n", type=int, default=10, help="Number of notes to list")
     parser.add_argument("-i", type=str, help="Query to ignore", nargs="+")
@@ -19,11 +29,15 @@ def main():
         files = filter(lambda f: not any([ignore in str(f) for ignore in args.i]), files)
     if args.q:
         files = filter(lambda f: any([query in str(f) for query in args.q]), files)
+    files = pd.DataFrame(files, columns=['path'])
 
-    sized_files = ((f, f.stat().st_size / 1024) for f in files)
-    sorted_files = sorted(sized_files, key=lambda x: x[1], reverse=True)[: args.n]
-    for fn, size in sorted_files:
-        print(f"{size:4.2f} kb -- {fn}")
+    files['stem'] = files['path'].apply(lambda x: x.stem)
+    files['parents'] = files['path'].apply(get_parents)
+    files['kb'] = files['path'].apply(get_kb)
+    blank = [''] * len(files)
+    files = files.sort_values(by='kb', ascending=False)
+    files.index = blank
+    print(files[['kb', 'stem', "parents"]].head(args.n))
 
 
 if __name__ == "__main__":
