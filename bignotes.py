80e6c7be1840b85eed712362ecf8e692b8b2e0c5
Chris Davison <c.jr.davison@gmail.com>
2019-05-06 20:10:38 +0100

    Simplify bignotes filtering

--- bignotes.py
+++ bignotes.py
@@ -5,39 +5,37 @@ from functools import partial
 
 import pandas as pd
 from toolz.functoolz import *
-
-
-def get_parents(path):
-    return '/'.join([str(s) for s in path.parents][::-1][1:])
+from IPython.display import display
 
 
 def get_kb(path):
     return int(path.stat().st_size / 1024)
 
 
+def matches_any(queries, path):
+    return any([query in str(path) for query in queries])
+
+
 def main():
     """Find largest markdown files under current directory"""
     parser = ArgumentParser("bignotes")
     parser.add_argument("-n", type=int, default=10, help="Number of notes to list")
-    parser.add_argument("-i", type=str, help="Query to ignore", nargs="+")
-    parser.add_argument("-q", type=str, help="Query to find", nargs="+")
+    parser.add_argument("-i", help="Query to ignore", nargs="+", default=["zzzzz"])
+    parser.add_argument("-q", help="Query to find", nargs="+", default=[""])
+    parser.add_argument("-f", help="File with processed files")
     args = parser.parse_args()
 
-    files = Path(".").rglob("*.md")
-
-    if args.i:
-        files = filter(lambda f: not any([ignore in str(f) for ignore in args.i]), files)
-    if args.q:
-        files = filter(lambda f: any([query in str(f) for query in args.q]), files)
+    queries_to_ignore = args.i
+    if args.f:
+        queries_to_ignore.extend(Path(args.f).read_text().splitlines())
+    files = [f for f in Path(".").rglob("*.md")
+             if matches_any(args.q, f)
+             and not matches_any(queries_to_ignore, f)]
     files = pd.DataFrame(files, columns=['path'])
-
-    files['stem'] = files['path'].apply(lambda x: x.stem)
-    files['parents'] = files['path'].apply(get_parents)
     files['kb'] = files['path'].apply(get_kb)
-    blank = [''] * len(files)
     files = files.sort_values(by='kb', ascending=False)
-    files.index = blank
-    print(files[['kb', 'stem', "parents"]].head(args.n))
+    for _, row in files.iloc[:args.n].iterrows():
+        print(f"({row.kb}) {row.path.name}")
 
 
 if __name__ == "__main__":
