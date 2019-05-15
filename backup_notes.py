e7f5e01a04a3969b1da1c8ea9d8ae5458eb65388
Chris Davison <c.jr.davison@gmail.com>
2019-05-15 21:57:58 +0100

    Expand tilde in note backup

--- backup_notes.py
+++ backup_notes.py
@@ -9,12 +9,12 @@ from pathlib import Path
 
 def main(outdir, note_format="*.org"):
     """Copy all notes with format FORMAT to outdir."""
-    dropbox = Path('e:/Dropbox/')
+    dropbox = Path('~/Dropbox/').expand_user()
 
     files = [f for f in dropbox.rglob(note_format) if 'dropbox.cache' not in str(f)]
     files.extend([f for f in dropbox.rglob('*') if 'assets' in str(f) and f.is_file()])
 
-    outdir = Path('e:/Downloads/standalone-notes-backup/')
+    outdir = Path('~/Downloads/standalone-notes-backup/').expand_user()
 
     for path in files:
         out = outdir / path.relative_to(dropbox)
@@ -28,3 +28,4 @@ def main(outdir, note_format="*.org"):
 if __name__ == '__main__':
     TODAY = date.today().strftime("%Y-%m-%d")
     main(outdir=Path(f"~/Downloads/standalone-notes-backup--{TODAY}"))
+    print(f"Notes backed up to {outdir}")
