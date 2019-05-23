a931ee92afe4d2282f56d321dbe0262dc6bd659c
Chris Davison <c.jr.davison@gmail.com>
2019-05-23 13:58:44 +0100

    Convert asmr to use json

--- asmr.py
+++ asmr.py
@@ -2,9 +2,10 @@
 """ASMR video management
 
 Usage:
-    asmr play [-far] [<query>]...
-    asmr view [-fa] [<query>]...
+    asmr play [-r] [<query>]...
+    asmr view [<query>]...
     asmr modify [<query>]...
+    asmr delete [<query>]...
     asmr add
 
 Commands:
@@ -12,37 +13,55 @@ Commands:
     view    List all videos matching query
     modify  Change metadata of an existing video
     add     Add a new video to the database
+    delete  Delete a video
 
 Options:
-    -f --favourites      Display only favourites      [default: False]
-    -a --with-archived   Also display archived videos [default: False]
     -r --random          Choose a random video        [default: False]
 """
 import os
+import json
 import re
-import sqlite3
 import webbrowser
-from collections import namedtuple
 from pathlib import Path
 from random import choice as random_choice
 
 from docopt import docopt
 
 
-Video = namedtuple("Video", "title artist vid_id fav archived")
-DB_FILENAME = str((Path(os.environ["DATADIR"]) / "data.db").resolve())
+FILENAME = str((Path(os.environ["DATADIR"]) / "asmr.json").resolve())
+
+
+def write_sorted_videos(videos):
+    sorted_videos = sorted(videos, key=lambda x: x['artist'])
+    json.dump(sorted_videos, open(FILENAME, 'w'), indent=2)
+
+
+def read_videos():
+    return json.load(open(FILENAME, 'r'))
+
+
+def contains(v, query):
+    query = query.lower()
+    return query in v['artist'].lower() or query in v['title'].lower()
+
+
+def new_video():
+    title = input("Title: ")
+    artist = input("Artist: ")
+    url = input("URL: ")
+    if not (url.startswith('http') or url.startswith('www.')):
+        url = f"https://www.youtube.com/watch?v={url}"
+    return {'title': title, 'artist': artist, 'url': url}
 
 
 def format_video(video):
     """Prettyprint a Video"""
-    fav = "F" if video.fav else "."
-    arc = "@" if video.archived else "."
-    return f"{fav} {arc} {video.artist:20s}{video.title}"
+    return f"{video['artist']:20s}{video['title']}"
 
 
 def display(entries):
     """For every asmr entry, display it's index and a pretty printed title"""
-    title = f"   # F A {'ARTIST'.ljust(20)}TITLE"
+    title = f"   # {'ARTIST'.ljust(20)}TITLE"
     print(title)
     print("-" * (len(title) + 20))
     for idx, entry in enumerate(entries):
@@ -64,120 +83,96 @@ def choose(entries, random=False):
     return [entries[c] for c in choice]
 
 
-def select_videos(only_favourites, with_archived, query):
-    """Run a database query for videos"""
-    fav = "AND fav = 1" if only_favourites else ""
-    archived = "AND archived = 0" if not with_archived else ""
-    database = sqlite3.connect(DB_FILENAME)
-    cursor = database.cursor()
-    cursor.execute(
-        f"""
-        select * from asmr
-        where (title like '%{query}%' or artist like '%{query}%')
-            {fav} {archived}
-        ORDER BY artist, fav DESC, title"""
-    )
-    data = cursor.fetchall()
-    database.close()
-    return [Video(*d) for d in data]
+def select_videos(query):
+    """Load and filter videos"""
+    videos = read_videos()
+    return [v for v in videos if contains(v, query)]
+
+
+def urlify(url):
+    if not url.startswith('http') and not url.startswith('www'):
+        return f"https://www.youtube.com/watch?v={url}"
+    return url
 
 
 def add(**kwargs):
     """Give the user inputs to add a new video to the ASMRFILE"""
     artist = input("Artist: ")
     title = input("Title: ")
-    vid = input("Video ID: ")
-    fav = 1 if input("Fav [y/n]: ")[0] in ["Y", "y"] else 0
+    url= urlify(input("Video ID: "))
 
-    if vid.startswith("www.") or vid.startswith("http"):
-        match = re.search(".*?video=(.{11}).*", vid)
-        if match:
-            vid = match.group(1)
-    if not vid or len(vid) != 11:
-        raise Exception("add: Could not parse video id")
+    videos = read_videos()
+    videos.append({'title': title, 'artist': artist, 'url': url})
+    write_sorted_videos(videos)
 
-    database = sqlite3.connect(DB_FILENAME)
-    cursor = database.cursor()
-    cursor.execute(
-        f"""
-        INSERT INTO asmr(title, artist, hash, fav, archived)
-        VALUES ( '{title}', '{artist}', '{vid}', '{fav}', '0' )
-    """
-    )
-    database.commit()
-    database.close()
 
 
 def modify(*, query, **kwargs):
     """Modify an existing video's metadata"""
+
     query = " ".join(query).lower()
-    only_favourites, with_archived = False, True
-    videos = select_videos(only_favourites, with_archived, query)
-    video = choose(videos, False)
-    print(format_video(video))
-    print("Enter new values, or leave blank to keep current")
-    updates = []
-    inp = input(f"artist ({video.artist}): ")
-    if inp:
-        updates.append(f"artist='{inp}'")
-    inp = input(f"title ({video.title}): ")
-    if inp:
-        updates.append(f"title='{inp}'")
-    inp = input(f"hash ({video.vid_id}): ")
-    if inp:
-        updates.append(f"hash='{inp}'")
-    inp = input(f"fav ({video.fav}) (1/0): ")
-    if inp:
-        updates.append(f"fav={int(inp)}")
-    inp = input(f"archived ({video.archived}) (1/0): ")
-    if inp:
-        updates.append(f"archived={int(inp)}")
+    videos = read_videos()
+    for i, video in enumerate(videos):
+        if contains(video, query):
+            print(f"{i:5d}\t{format_video(video)}")
+    choice = int(input("Choice: "))
 
-    if not updates:
-        return
+    print("\nEnter new values, or press enter to keep current value")
 
-    database = sqlite3.connect(DB_FILENAME)
-    cursor = database.cursor()
-    command = f"""
-        UPDATE asmr
-        SET {','.join(updates)}
-        WHERE artist={video.artist} and title={video.title}
-    """
-    cursor.execute(command)
-    database.commit()
+    def prompt_or_stay_same(prompt, current):
+        val = input(f"\t{prompt} ({current}): ")
+        if not val:
+            return current
+        return val
+
+    videos[choice]['artist'] = prompt_or_stay_same("Artist", videos[choice]['artist'])
+    videos[choice]['title'] = prompt_or_stay_same("Title", videos[choice]['title'])
+    videos[choice]['url'] = urlify(prompt_or_stay_same("URL", videos[choice]['url']))
+    videos[choice]['broken'] = prompt_or_stay_same("Broken?", videos[choice]['broken'])
+    write_sorted_videos(videos)
 
 
-def view(*, query, only_favourites, with_archived, **kwargs):
-    """List videos, optionally filtered by query or favourites only"""
+def delete(*, query, **kwargs):
+    """Delete a video"""
     query = " ".join(query).lower()
-    videos = select_videos(only_favourites, with_archived, query)
+    videos = read_videos()
+    for i, video in enumerate(videos):
+        if contains(video, query):
+            print(f"{i:5d}\t{format_video(video)}")
+    choice = int(input("Choice: "))
+    if choice in range(len(videos)):
+        del videos[choice]
+    write_sorted_videos(videos)
+
+
+def view(*, query, **kwargs):
+    """List videos, optionally filtered by query"""
+    query = " ".join(query).lower()
+    videos = select_videos(query)
     display(videos)
 
 
-def play(*, query, random, only_favourites, with_archived):
+def play(*, query, random):
     """Play a video (optionally filtered)"""
     query = " ".join(query).lower()
-    videos = select_videos(only_favourites, with_archived, query)
+    videos = select_videos(query)
     choice = choose(videos, random)
     for video in choice:
         print(format_video(video))
-        url = f"https://www.youtube.com/watch?v={video.vid_id}"
-        webbrowser.open(url)
+        webbrowser.open(video['url'])
 
 
 def main():
     """Run asmr video util"""
     args = docopt(__doc__)
-    commands = [("play", play), ("view", view), ("modify", modify), ("add", add)]
+    commands = [("play", play), ("view", view), ("modify", modify), ("add", add), ("delete", delete)]
     # Due to the way docopt works, we should _always_ have one of the above commands
     # by the time we reach this point, so it's save to just take the 0th
     # Also, only one command should be available, so the list should be length 1.
     command = [func for (funcname, func) in commands if args[funcname]][0]
     command(
         query=args["<query>"],
-        random=args["--random"],
-        only_favourites=args["--favourites"],
-        with_archived=args["--with-archived"],
+        random=args["--random"]
     )
 
 
