d6c87fdae2cac65afeb5325c8186dab245c3b714
Chris Davison <c.jr.davison@gmail.com>
2019-06-06 15:23:52 +0100

    Git-record ignores stdout

--- git/git-record.sh
+++ git/git-record.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
-if [ -z $1 ]; then
-    echo "Must give commit message"
-    exit
-fi
-
 echo "Adding all, committing, and pushing"
 
-git add .
-git commit -m "$@"
-git push
+git add . > /dev/null
+git commit -m "$@" > /dev/null
+git push > /dev/null
+
+echo "FINISHED"
