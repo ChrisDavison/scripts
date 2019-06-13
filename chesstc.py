ae861c876fcde25fcceca16bfa0b65f90c2ae07c
Chris Davison <c.jr.davison@gmail.com>
2019-06-13 22:52:13 +0100

    Remove click dependency from chesstc

--- chesstc.py
+++ chesstc.py
@@ -1,19 +1,27 @@
 #! /usr/bin/env python
-import click
+"""chesstc - Calculate expected chess game time.
 
+usage: chesstc <BASE> <INCREMENT>
 
-@click.command("timecontrol")
-@click.argument("base", nargs=1, type=float)
-@click.argument("increment", nargs=1, type=float)
+This calculates the expected duration of a chess game (Min:Sec) for a given
+base and increment; e.g. 5+0 should take approx 10 minutes.
+
+Assumes 40 moves, using a given base and increment.  Each player will have
+half of this duration each."""
+import sys
+
 def timecontrol(base, increment):
-    """Calculate expected chess game time.
 
-    Assumes 40 moves, using a given base and increment.  Each player will have
-    half of this duration each."""
     totalsec = (float(base) * 60 + float(increment) * 40) * 2
     minutes = int(totalsec / 60)
     remsec = int(totalsec - minutes * 60)
     print(f"{minutes}:{remsec}")
 
 if __name__ == "__main__":
-    timecontrol()
+    args = sys.argv[1:]
+    try:
+        base = float(args[0])
+        increment = float(args[1])
+        timecontrol(base, increment)
+    except:
+        print(__doc__)
