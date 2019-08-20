ee4482d8633bb04e596ce04a0d07102582ea1553
Chris Davison <c.jr.davison@gmail.com>
2019-08-20 09:36:28 +0100

    Add help message to sanitise

--- sanitise.sh
+++ sanitise.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+# Tidy up a filename
+if [ $# -eq 0 ]; then
+    echo "sanitise <filename> - convert to lowercase; keep alphanumeric, dots and dashes"
+    exit
+fi
 direc=$(dirname $1)
 base=$(basename $1)
 echo $base | tr '[:upper:]' '[:lower:]' | sed 's/[^a-zA-Z0-9.-]/-/g' | tr -s - - | sed 's/\-$//g'
