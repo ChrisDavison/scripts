c74e1c80e785d4dc5ac7a274339a48888d0f3b24
Chris Davison <c.jr.davison@gmail.com>
2019-09-06 13:37:57 +0100

    Script to give overview of ideas note

---
+++ ideas.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import re
+from argparse import ArgumentParser
+from collections import defaultdict
+
+
+parser = ArgumentParser('ideas')
+parser.add_argument('headline', nargs='*', help='Headline to show ideas for')
+args = parser.parse_args()
+
+filepath = Path('~/Dropbox/notes/idea-index.md').expanduser()
+contents = filepath.read_text().splitlines()
+
+headline = args.headline[0].lower() if args.headline else None
+
+headline_and_contents = defaultdict(list)
+header = None
+for line in contents:
+    re_header = re.match(r'(#+)\s+(.*)', line)
+    re_line = re.match(r'-\s+(.*)', line)
+    if re_header and len(re_header.group(1)) > 1:
+        header = re_header.group(2).lower()
+    elif header and re_line:
+        headline_and_contents[header].append(re_line.group(1))
+
+if headline and headline in headline_and_contents.keys():
+    for entry in headline_and_contents[headline]:
+        print(f"- {entry}")
+else:
+    print("SECTIONS AVAILABLE")
+    print("==================")
+    print('\n'.join(sorted(headline_and_contents.keys())))
+
+# for indent, text in headers:
+#     indent = '-'*(indent-2)
+#     print(f"{indent}{text}")
+
