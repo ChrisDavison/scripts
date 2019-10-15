87343c6eef16cc2cb5f4b35de45b17852b59bbb6
Chris Davison <c.jr.davison@gmail.com>
2019-10-15 12:40:52 +0100

    Add budget flag to show archive

--- budget.py
+++ budget.py
@@ -19,17 +19,20 @@ def process_dir(path):
         output += f"{cost:8.0f}\t{name:10s}\n"
     print(f"TOTAL for {path.stem} -- {summed:.1f}")
     if args.verbose:
-        print(output)
+        sorted_lines = sorted(line for line in output.split('\n'))
+        print('\n'.join(sorted_lines[::-1]))
 
 
 parser = ArgumentParser('budget')
 parser.add_argument('directory', nargs='?', help="Directory with costs")
 parser.add_argument('-v', '--verbose', help="Show all costs", action='store_true')
+parser.add_argument('-a', '--archive', help="Also show archive", action='store_true')
 args = parser.parse_args()
 
 root = os.environ['FINANCES']
 if args.directory:
     root = args.directory
+    dirs = [Path(root)]
 elif root:
     dirs = [d for d in Path(root).glob('*') if d.is_dir()]
 else:
@@ -37,4 +40,6 @@ else:
     sys.exit(1)
 
 for direc in dirs:
+    if 'archive' in str(direc) and not args.archive:
+        continue
     process_dir(direc)
