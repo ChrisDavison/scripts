190f2ee62287e6b7cfa110503ae413542d55a6f7
Chris Davison <c.jr.davison@gmail.com>
2019-10-16 22:21:39 +0100

    Stop overcomplicating budget...

--- budget.py
+++ budget.py
@@ -10,44 +10,40 @@ from pathlib import Path
 RE_KEYWORD = re.compile(r"([a-zA-Z]+): (.*)")
 
 
-BudgetDir = namedtuple('BudgetDir', 'directory budget_items')
+BudgetItem = namedtuple('BudgetItem', 'name cost date')
 
 
-class BudgetItem:
-    """BudgetItem represents something that money has been spent on."""
-    def __init__(self, name, cost, date):
-        self.name = name
-        self.cost = cost
-        self.date = date
+def parse_budget_item(filepath):
+    """Generate a BudgetItem from a file with key: value pairs"""
+    if not filepath.is_file():
+        return None
+    keywords = defaultdict(list)
+    for line in filepath.read_text().splitlines():
+        m = RE_KEYWORD.match(line)
+        if m:
+            keywords[m.group(1)].append(m.group(2))
+        elif line:
+            keywords['notes'].append(line.strip())
+    name = keywords.get('name', [None])[0]
+    cost = keywords.get('cost', [None])[0]
+    date = keywords.get('date', '')
+    return BudgetItem(name, float(cost), date)
 
-    def __str__(self):
-        return f"{int(self.cost):>8d} -- {self.name} {self.date}"
 
-    @staticmethod
-    def from_filepath(filepath):
-        """Generate a BudgetItem from a file with key: value pairs"""
-        if not filepath.is_file():
-            return None
-        keywords = defaultdict(list)
-        for line in filepath.read_text().splitlines():
-            m = RE_KEYWORD.match(line)
-            if m:
-                keywords[m.group(1)].append(m.group(2))
-            elif line:
-                keywords['notes'].append(line.strip())
-        name = keywords.get('name', [None])[0]
-        cost = keywords.get('cost', [None])[0]
-        date = keywords.get('date', '')
-        return BudgetItem(name, float(cost), date)
+def budget_item_repr(budget_item):
+    date = f" ({budget_item.date})" if budget_item.date else ''
+    return f"{int(budget_item.cost):>8d} -- {budget_item.name}{date}"
 
 
 def process_dir(path, verbose):
-    budgets = [BudgetItem.from_filepath(f) for f in path.glob('*')]
+    if not isinstance(path, Path):
+        path = Path(path)
+    budgets = [parse_budget_item(f) for f in path.glob('*')]
     summed = sum([budget.cost for budget in budgets])
-    print(f"{path.stem:8s} ~ {int(summed)}")
+    print(f"{path.stem:8s} ~ £{int(summed)}")
     if verbose:
         for budget in sorted(budgets, key=lambda x: x.cost)[::-1]:
-            print(budget)
+            print(budget_item_repr(budget))
 
 
 parser = ArgumentParser('budget')
