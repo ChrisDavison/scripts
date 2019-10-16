5220ada901a0047b3927907bcbf0f90fcde1bc0c
Chris Davison <c.jr.davison@gmail.com>
2019-10-16 14:59:45 +0100

    WIP class for budget

--- budget.py
+++ budget.py
@@ -3,11 +3,54 @@ import os
 import re
 import sys
 from argparse import ArgumentParser
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from pathlib import Path
 
 
 RE_KEYWORD = re.compile(r"([a-zA-Z]+): (.*)")
+
+
+BudgetItem = namedtuple('BudgetItem', 'name cost date notes')
+BudgetDir = namedtuple('BudgetDir', 'directory budget_items')
+
+class BudgetItem:
+    def __init__(self, name, cost, date, notes, extra):
+        self.name = name
+        self.cost = cost
+        self.date = date
+        self.notes = notes
+        self.extra = extra
+
+    def __str__(self):
+        return f"{int(self.cost):>8d} -- {self.name} {self.date}"
+
+    def __repr__(self):
+        output = f"{int(self.cost):>8d} -- {self.name} {self.date}"
+        for note in self.notes:
+            output += "\t" + note
+        for kw, val in self.extra:
+            output += "\t" + kw + ": " + val
+        return output
+
+    @staticmethod
+    def from_filepath(filepath):
+        if not filepath.is_file():
+            return None
+        keywords = defaultdict(list)
+        name, cost, date, notes = None, 0.0, "", []
+        for line in filepath.read_text().splitlines():
+            m = RE_KEYWORD.match(line)
+            if m and m.group(1) == 'name':
+                name = m.group(2)
+            elif m and m.group(1) == 'cost':
+                cost = float(m.group(2))
+            elif m and m.group(1) == 'date':
+                date = m.group(2)
+            elif m:
+                keywords[m.group(1)].append(m.group(2))
+            elif line:
+                notes.append(line)
+        return BudgetItem(name, cost, date, notes, extra)
 
 
 def parse_file(filename):
@@ -26,16 +69,7 @@ def parse_file(filename):
     return keywords
 
 
-# def budget_string_repr(file_keywords, verbose):
-#     name = keywords['name'][0]
-#     cost = float(keywords.get('cost', [0])[0])
-#     date = keywords.get('date', [''])[0]
-#     date = f"({date})" if date else ""
-
-
-
 def process_dir(path):
-    summed = 0
     output = []
     for file in path.glob('*'):
         current = []
@@ -44,12 +78,12 @@ def process_dir(path):
         cost = float(keywords.get('cost', [0])[0])
         date = keywords.get('date', [''])[0]
         date = f"({date})" if date else ""
-        summed += cost
         current.append(f"{int(cost):>8d} -- {name} {date}")
         if args.verbose:
             for line in keywords.get('notes', []):
                 current.append(f"\t\t\t{line}")
         output.append((cost, '\n'.join(current)))
+    summed = sum([cost for cost, _ in output])
     print(f"{path.stem:8s} ~ {int(summed)}")
     if args.verbose:
         sorted_outputs = sorted(output, key=lambda x: x[0], reverse=True)
