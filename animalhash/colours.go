c9a3fc2b6c40ff3fd3a8dbefc7240b72f0e586cf
Chris Davison <c.jr.davison@gmail.com>
2019-10-18 11:04:20 +0100

    Golang animalhash

---
+++ animalhash/colours.go
@@ -0,0 +1,32 @@
+package main
+
+const COLOURS = `aqua
+aquamarine
+black
+chartreuse
+cornflower
+cyan
+fuchsia
+gold
+green
+grey
+khaki
+lime
+magenta
+maroon
+navy
+olive
+orange
+pink
+purple
+red
+rose
+salmon
+silver
+tan
+teal
+turquoise
+violet
+wheat
+white
+yellow`
