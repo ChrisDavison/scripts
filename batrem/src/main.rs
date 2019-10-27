533c36cb5f72f8efe37507e8a0d3ebca574e818a
Chris Davison <c.jr.davison@gmail.com>
2019-10-27 12:15:11 +0000

    Run rustfmt on batrem

--- batrem/src/main.rs
+++ batrem/src/main.rs
@@ -1,4 +1,4 @@
-use std::process::{Command,exit};
+use std::process::{exit, Command};
 
 fn main() {
     let output = Command::new("acpi").output().expect("couldn't run acpi");
@@ -7,11 +7,15 @@ fn main() {
         println!("B FULL");
         exit(1);
     }
-    let arrow = match battery_string.contains("Charging") {
-        true => "↑",
-        false => "↓",
+    let arrow = if battery_string.contains("Charging") {
+        "↑"
+    } else {
+        "↓"
     };
-    let remaining: &str = battery_string.split(", ").nth(2).expect("couldn't split acpi output");
-    let time: String = remaining.split(" ").take(1).collect::<String>()[..5].into();
-	println!("B{} {}", arrow, time);
+    let remaining: &str = battery_string
+        .split(", ")
+        .nth(2)
+        .expect("couldn't split acpi output");
+    let time: String = remaining.split(' ').take(1).collect::<String>()[..5].into();
+    println!("B{} {}", arrow, time);
 }
