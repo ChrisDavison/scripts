8352c361e70b181eb3e25164380489a017a9bf4c
Chris Davison <c.jr.davison@gmail.com>
2019-10-30 11:49:30 +0000

    Add argparse

--- count_note_headers.py
+++ count_note_headers.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 from pathlib import Path
+import re
 
-notes = list((Path('~').expanduser() / 'Dropbox' / 'notes').glob('*.org'))
+parser = ArgumentParser()
+parser.add_argument('files', help='files to count headers in', nargs='+')
+parser.add_argument('--sym', help='header symbol', default=b'#', type=bytes)
+args = parser.parse_args()
+
+
+notes = [Path(f) for f in args.files]
 
 filter_words = ['book-', 'logbook', 'asmr', 'gaming', 'programming']
 
 min_count = 5
 
-def header_count(note):
+def header_count(note, symbol):
+    re_header = re.compile(symbol + b"+ ")
     note_text = note.read_bytes().split(b'\n')
-    return len([l for l in note_text if l.startswith(b'*')])
+    lines_with_headers = sum(1 for line in note_text if re_header.match(line))
+    return lines_with_headers
 
-notes_and_count = [(header_count(n), n.stem) for n in notes]
+notes_and_count = [(header_count(n, args.sym), n.stem) for n in notes]
 sorted_notes = sorted(notes_and_count, reverse=True)
 
 for note in sorted_notes:
