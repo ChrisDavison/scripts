19d2c2af68cf7069e24a42fb70460be61cf8a9dc
Chris Davison <c.jr.davison@gmail.com>
2019-11-04 13:20:21 +0000

    Add 'radius' as arg to more latlong functions

--- latlong_geom.py
+++ latlong_geom.py
@@ -3,31 +3,29 @@
 
 This is approximate (i.e. it assumes perfect sphere).
 """
+import sys
 import numpy as np
 
 
 RADIUS_EARTH = 6317 # kilometres
 
 
-def to_radians(degrees):
-    return degrees * np.pi / 180
-
-
 def area_above_latitude(lat, radius=RADIUS_EARTH):
-    return 2 * np.pi * radius * radius * (1 - np.sin(to_radians(lat)))
+    lat_as_radians = lat * np.pi / 180
+    return 2 * np.pi * radius * radius * (1 - np.sin(lat_as_radians))
 
 
-def area_between_latitudes(lat1, lat2):
+def area_between_latitudes(lat1, lat2, radius=RADIUS_EARTH):
     lat1, lat2 = (lat1, lat2) if lat1 > lat2 else (lat2, lat1)
-    return area_above_latitude(lat2) - area_above_latitude(lat1)
+    return area_above_latitude(lat2, radius) - area_above_latitude(lat1, radius)
 
 
-def area_quadrangle(latlon1, latlon2):
+def area_quadrangle(latlon1, latlon2, radius=RADIUS_EARTH):
     lat1, lon1 = latlon1
     lat2, lon2 = latlon2
-    area_of_band = area_between_latitudes(lat1, lat2)
+    area_of_band = area_between_latitudes(lat1, lat2, radius)
     vertical_proportion_of_sphere = (lon2 - lon1) / 360
-    return area_of_band * vertical_proportion_of_sphere
+    return area_of_band * vertical_proportion_of_sphere2
 
 
 def area_earth():
