e3e1edd16d19fcff094d61abc079eaab0c474019
Chris Davison <c.jr.davison@gmail.com>
2019-12-11 10:31:15 +0000

    Fix latlong_to_ddd to accept south and west DMS

--- latlong_to_ddd.go
+++ latlong_to_ddd.go
@@ -6,34 +6,54 @@ import (
 	"strconv"
 )
 
+const USAGE = `latlong_to_ddd - convert degrees-minutes-seconds to decimal-degrees
+
+usage:
+	latlong_to_ddd [-s -w] D_north M_north S_north D_east M_east S_east
+
+options:
+	-s    First degree-minutes-seconds is actually South
+	-w    Second degree-minutes-seconds is actually West
+`
+
+func toDecimalDegrees(degrees, minutes, seconds float64) float64 {
+	minPlusSec := (minutes + (seconds / 60))
+	return degrees + (minPlusSec / 60)
+}
+
 func main() {
-	args := os.Args[1:]
+	args := make([]float64, 0)
+	isSouth := false
+	isWest := false
+
+	for _, arg := range os.Args[1:] {
+		if arg == "-s" {
+			isSouth = true
+		} else if arg == "-w" {
+			isWest = true
+		} else {
+			val, err := strconv.ParseFloat(arg, 10)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error parsing `%s`: %s\n", arg, err)
+				os.Exit(1)
+			}
+			args = append(args, val)
+		}
+	}
 	if len(args) != 6 {
-		fmt.Fprintln(os.Stderr, "usage: latlong_to_ddd D_north M_north S_north D_east M_east S_east")
-		os.Exit(1)
+		fmt.Fprintln(os.Stderr, "Not enough args.")
+		fmt.Println(USAGE)
+		os.Exit(2)
 	}
-	d_north, _ := strconv.ParseFloat(args[0], 10)
-	m_north, _ := strconv.ParseFloat(args[1], 10)
-	if m_north < 0 {
-		m_north *= -1
-	}
+	d_north, m_north, s_north, d_east, m_east, s_east := args[0], args[1], args[2], args[3], args[4], args[5]
 
-	s_north, _ := strconv.ParseFloat(args[2], 10)
-	if s_north < 0 {
-		s_north *= -1
+	ddd_north := toDecimalDegrees(d_north, m_north, s_north)
+	ddd_east := toDecimalDegrees(d_east, m_east, s_east)
+	if isSouth {
+		ddd_north *= -1
 	}
-
-	d_east, _ := strconv.ParseFloat(args[3], 10)
-	m_east, _ := strconv.ParseFloat(args[4], 10)
-	if m_east < 0 {
-		m_east *= -1
+	if isWest {
+		ddd_east *= -1
 	}
-	s_east, _ := strconv.ParseFloat(args[5], 10)
-	if s_east < 0 {
-		s_east *= -1
-	}
-
-	ddd_north := d_north + ((m_north + (s_north / 60)) / 60)
-	ddd_east := d_east + ((m_east + (s_east / 60)) / 60)
 	fmt.Printf("%f,%f\n", ddd_north, ddd_east)
 }
