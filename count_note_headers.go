7d176d63ee99da735fe442f08bdf17c9729d94c2
Chris Davison <c.jr.davison@gmail.com>
2020-02-03 10:37:53 +0000

    Simplify count_note_headers implementation

--- count_note_headers.go
+++ count_note_headers.go
@@ -22,77 +22,54 @@ func (c countedFile) String() string {
 	return fmt.Sprintf("%-5d %s", c.count, c.filename)
 }
 
-type countedFiles []countedFile
-
-func (cf countedFiles) Less(i, j int) bool {
-	return cf[i].count < cf[j].count
-}
-
-func (cf countedFiles) Swap(i, j int) {
-	cf[i], cf[j] = cf[j], cf[i]
-}
-
-func (cf countedFiles) Len() int {
-	return len(cf)
-}
-
-const (
-	NoFilesGlobFail int = iota
-	PassedPatternGlobFail
-)
-
 func main() {
-	var files []string
-	var err error
-	if len(os.Args) == 1 {
-		files, err = doublestar.Glob("**/*.txt")
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(NoFilesGlobFail)
-		}
-	} else if len(os.Args) == 2 && strings.HasPrefix(os.Args[1], "*") {
-		files, err = doublestar.Glob(os.Args[1])
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(PassedPatternGlobFail)
-		}
-	} else {
-		files = os.Args[1:]
+	files, err := getFiles(os.Args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	minCount := 5
 	symbols := map[string]string{
 		"md":  "#",
 		"txt": "#",
 		"org": "*",
 	}
-	filterWords := []string{"book-", "logbook", "asmr", "gaming", "programming"}
-	var countedFileList countedFiles
+
+	var countedFileList []countedFile
 	for _, file := range files {
-		hasFilterWord := false
-		for _, fw := range filterWords {
-			if strings.Contains(file, fw) {
-				hasFilterWord = true
-				break
-			}
-		}
-		if hasFilterWord {
-			continue
-		}
 		sym := symbols[path.Ext(file)]
 		count, err := headerCount(file, sym)
 		if err != nil {
 			os.Exit(1)
 		}
-		if count > minCount {
-			countedFileList = append(countedFileList, countedFile{file, count})
-		}
+		countedFileList = append(countedFileList, countedFile{file, count})
 	}
-	sort.Sort(sort.Reverse(countedFileList))
 
+	sort.Slice(
+		countedFileList,
+		func(i, j int) bool {
+			return countedFileList[i].count > countedFileList[j].count
+		},
+	)
+
+	minCount := 0
+	// filterWords := []string{"book-", "logbook", "asmr", "gaming", "programming"}
+	filterWords := []string{}
 	for _, cf := range countedFileList {
+		if containsAny(cf.filename, filterWords) || (minCount > 0 && cf.count > minCount) {
+			continue
+		}
 		fmt.Println(cf)
 	}
+}
+
+func containsAny(s string, filters []string) bool {
+	for _, fw := range filters {
+		if strings.Contains(s, fw) {
+			return true
+		}
+	}
+	return false
 }
 
 func headerCount(file, headerSymbol string) (int, error) {
@@ -109,4 +86,20 @@ func headerCount(file, headerSymbol string) (int, error) {
 		}
 	}
 	return len(linesWithHeaders), nil
+}
+
+func getFiles(args []string) ([]string, error) {
+	var files []string
+	var err error
+	if len(os.Args) == 1 {
+		files, err = doublestar.Glob("**/*.txt")
+		if err != nil {
+			return []string{}, fmt.Errorf("Error globbing doublestar: %w", err)
+		}
+	} else if len(os.Args) > 1 {
+		files = os.Args[1:]
+	} else {
+		files = os.Args[1:]
+	}
+	return files, nil
 }
