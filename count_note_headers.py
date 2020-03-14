1d6847a64ad3c84869912a085326281c781b153d
Chris Davison <c.jr.davison@gmail.com>
2020-03-14 16:42:28 +0000

    Rewrite latlong and note header scripts in python

---
+++ count_note_headers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import re
+from os.path import basename, splitext
+from argparse import ArgumentParser
+
+SYMBOLS = {'md': b'#', 'txt': b'#', 'org': b'*'}
+
+
+def count_headers(filename):
+    extension = splitext(filename)[1][1:]
+    symbol = SYMBOLS[extension]
+    rx = re.compile(b"^"  + symbol)
+    data = open(filename, 'rb').read().split(b"\n")
+    headers = [line for line in data if rx.match(line)]
+    return (len(headers), filename)
+
+
+if __name__ == '__main__':
+    PARSER = ArgumentParser()
+    PARSER.add_argument("files", nargs='+')
+    PARSER.add_argument("--min-count", default=0, type=int)
+    ARGS = PARSER.parse_args()
+
+    counted = [count_headers(filename) for filename in ARGS.files]
+    
+    for (count, filename) in sorted(counted)[::-1]:
+        if count >= ARGS.min_count:
+            print("{:10} {}".format(count, basename(filename)))
+            
--- latlong_geom.py
+++ latlong_geom.py
@@ -5,6 +5,7 @@ This is approximate (i.e. it assumes perfect sphere).
 """
 import sys
 import numpy as np
+from argparse import ArgumentParser
 
 
 RADIUS_EARTH = 6317 # kilometres
@@ -34,3 +35,26 @@ def area_earth():
 
 def area_quadrangle_as_proportion_of_earth(latlon1, latlon2):
     return area_quadrangle(latlon1, latlon2) / area_earth()
+
+
+def to_decimal_degrees(dms_north, dms_east):
+    degrees, minutes, seconds = dms_north
+    dd_north = degrees + (minutes + (seconds / 60)) / 60
+
+    degrees, minutes, seconds = dms_east
+    dd_east = degrees + (minutes + (seconds / 60)) / 60
+    print("{:.3f},{:.3f}".format(dd_north, dd_east))
+
+
+if __name__ == "__main__":
+    PARSER = ArgumentParser()
+    PARSER.add_argument("d_north", type=float)
+    PARSER.add_argument("m_north", type=float)
+    PARSER.add_argument("s_north", type=float)
+    PARSER.add_argument("d_east", type=float)
+    PARSER.add_argument("m_east", type=float)
+    PARSER.add_argument("s_east", type=float)
+    ARGS = PARSER.parse_args()
+    dms_north = (ARGS.d_north, ARGS.m_north, ARGS.s_north)
+    dms_east = (ARGS.d_east, ARGS.m_east, ARGS.s_east)
+    to_decimal_degrees(dms_north, dms_east)
