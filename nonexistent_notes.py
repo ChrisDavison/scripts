f6ecc53f096558bd68049564ed9404d62bdd1ae7
Chris Davison <c.jr.davison@gmail.com>
2020-04-16 22:53:35 +0100

    Flatten nesting in nonexistent_notes

--- nonexistent_notes.py
+++ nonexistent_notes.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-import os 
-import re 
+import os
+import re
 from argparse import ArgumentParser
 
 
@@ -21,18 +21,19 @@ def get_links_in_file(filename):
             links.append((i, m.span()[0], m.group(1)))
     return links
 
-    
+
 def print_broken_forwardlinks(filename, vimgrep_output):
     broken = [(i, j, link) for (i, j, link) in get_links_in_file(filename)
             if not os.path.exists(os.path.join(os.path.dirname(filename), link))]
-    if broken:
-        if vimgrep_output:
-            for linum, colnum, link in broken:
-                print("{}:{}:{}:{}".format(filename, linum+1, colnum+1, link))
-        else:
-            print(filename)
-            for linum, colnum, link in broken:
-                print("  >", link)
+    if not broken:
+        return
+    if vimgrep_output:
+        for linum, colnum, link in broken:
+            print("{}:{}:{}:{}".format(filename, linum+1, colnum+1, link))
+    else:
+        print(filename)
+        for linum, colnum, link in broken:
+            print("  >", link)
 
 
 files = [f for f in os.listdir() if f.endswith(".md")]
@@ -40,4 +41,3 @@ if args.files:
     files = args.files
 for filename in files:
     print_broken_forwardlinks(filename, args.vimgrep)
-
