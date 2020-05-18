d8c040b67008a96e6496b24af4f72c215908d87c
Chris Davison <c.jr.davison@gmail.com>
2020-05-18 13:22:48 +0100

    kindle: enumerate highlights in org format

--- kindle_html_parser.py
+++ kindle_html_parser.py
@@ -1,25 +1,23 @@
+"""Parse highlights from a kindle"""
 import sys
 from bs4 import BeautifulSoup
 
+
 data = open(sys.argv[1], 'r').read()
 soup = BeautifulSoup(data, 'html.parser')
-
-notes_and_highlights = [h for h in soup.find_all(id=['highlight', 'note'])]
-
-def tidy(note):
-    return note.text.replace('\n', ' ')
-
+notes_and_highlights = soup.find_all(id=['highlight', 'note'])
 h3 = soup.find('h3').text
-fname = h3.replace(' ', '-') + '.txt'
+fname = h3.replace(' ', '-') + '.org'
 with open(fname.lower(), 'w') as f:
-    print('#', h3, file=f)
+    print('#+TITLE:', h3 + ' notes', file=f)
+    print('', file=f)
+    print('*', h3, file=f)
     print('', file=f)
 
-
-    for n in notes_and_highlights:
-        tidied = tidy(n)
+    for i, n in enumerate(notes_and_highlights):
+        tidied = n.text.replace('\n', ' ')
+        print('** highlight {}'.format(i), file=f)
         if n['id'] == 'note' and tidied:
             print('NOTE: ', end=' ', file=f)
-        print(tidy(n), file=f)
+        print(tidied, file=f)
         print('', file=f)
-        
