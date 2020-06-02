3dd781732bd60ae637c7aa69e3c5fb7ec8a04520
Chris Davison <c.jr.davison@gmail.com>
2020-06-02 20:02:27 +0100

    Improve capture template for web references

--- capture/liturl.py
+++ capture/liturl.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Reference for a website, in IEEE format"""
 import dateutil.parser as dp
 import pyperclip
+import os
 
 authors = []
 while True:
@@ -10,8 +12,10 @@ while True:
     authors.append(author)
 title = input("Title: ")
 url = input("URL: ")
+publisher = input("Publisher: ")
 year = input("Year of publication: ")
 accessed = dp.parse(input("Date accessed: "))
+doi = input("DOI: ")
 
 authors = [a.title() for a in authors]
 if len(authors) > 1:
@@ -19,8 +23,17 @@ if len(authors) > 1:
 
 formatted_authors = ', '.join(authors)
 
-msg = """{formatted_authors}, '{title}', {year}, {url} Accessed: {access_date}""".format(formatted_authors=formatted_authors, title=title, year=year, url=url, access_date=accessed.strftime("%d %b %Y"))
+msg = formatted_authors
+msg += ", '{}'".format(title)
+if year:
+    msg += ", {}".format(year)
+if publisher:
+    msg += ", {}".format(publisher)
+msg += ", {} Accessed: {}".format(url, accessed.strftime("%d %b %Y"))
+if doi:
+    msg += ", doi: {}".format(doi)
 
+os.system('cls' if os.name == 'nt' else 'clear')
+print("SAVED TO CLIPBOARD")
 print(msg)
 pyperclip.copy(msg)
-print('...saved to clipboard')
