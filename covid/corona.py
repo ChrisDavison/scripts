524de5879daa7de8a197af428c49f86b3c46070a
Chris Davison <c.jr.davison@gmail.com>
2020-06-06 17:31:34 +0100

    covid: couple days number, and join plots together

--- covid/corona.py
+++ covid/corona.py
@@ -21,7 +21,9 @@ DATED_NUMBER = namedtuple("DatedNumber", "date value")
 
 def get_todays_number(last_num):
     rx_number = re.compile("([0-9,]+).*were positive")
-    response = requests.get("https://www.gov.scot/publications/coronavirus-covid-19-daily-data-for-scotland/").text
+    response = requests.get(
+        "https://www.gov.scot/publications/coronavirus-covid-19-daily-data-for-scotland/"
+    ).text
     covid_number = rx_number.search(response)
     if covid_number:
         num = int(covid_number.group(1).replace(",", ""))
@@ -44,7 +46,9 @@ def get_numbers():
     numbers = []
     for line in open(NUMBER_FILE, "r"):
         date, num = line.split(",")
-        numbers.append(DATED_NUMBER(datetime.datetime.strptime(date, "%Y-%m-%d").date(), int(num)))
+        numbers.append(
+            DATED_NUMBER(datetime.datetime.strptime(date, "%Y-%m-%d").date(), int(num))
+        )
     is_before_2pm = datetime.datetime.now().hour < 14
     # Check if a new number exists
     todays_number = get_todays_number(last_num=numbers[-1])
@@ -57,9 +61,7 @@ def get_numbers():
 
 def predict_average(numbers):
     """Mean of growthrates"""
-    delta_days = [
-        (n1.date - n0.date).days for n0, n1 in zip(numbers, numbers[1:])
-    ]
+    delta_days = [(n1.date - n0.date).days for n0, n1 in zip(numbers, numbers[1:])]
     ratios = [n1.value / n0.value for n0, n1 in zip(numbers, numbers[1:])]
     rates = [ratio / delta for (delta, ratio) in zip(delta_days, ratios)]
     growthrate = sum(rates) / len(rates)
@@ -75,10 +77,8 @@ def predict_growthrate_poly(numbers, degrees):
     """Polyfit on growthrates"""
     date_0 = numbers[0].date
     days_since_0 = np.array([(n.date - date_0).days for n in numbers][1:])
-    delta_days = [
-        (n1.date - n0.date).days for n0, n1 in zip(numbers, numbers[1:])
-    ]
-    ratios = np.array([n1.value / n0.value  for n0, n1 in zip(numbers, numbers[1:])])
+    delta_days = [(n1.date - n0.date).days for n0, n1 in zip(numbers, numbers[1:])]
+    ratios = np.array([n1.value / n0.value for n0, n1 in zip(numbers, numbers[1:])])
     rates = [ratio / delta for (delta, ratio) in zip(delta_days, ratios)]
     model = np.poly1d(np.polyfit(days_since_0, rates, degrees))
     growthrate = model(days_since_0[-1] + 1)
@@ -131,55 +131,77 @@ def get_predictions(numbers):
 def nth(n):
     def nth_n(indexable):
         return indexable[n]
+
     return nth_n
 
 
 def print_predictions(message, numbers, last, closest_marker):
-        outfmt = "{}{} - {:12s} ({:.3f})"
-        print("\n" + message)
-        predictions = get_predictions(numbers)
-        index_closest = index_of_nearest([p for p, _, _ in predictions], last.value)
-        for i, (val, description, growth) in enumerate(predictions):
-            print(outfmt.format(closest_marker if i == index_closest else "  ", val, description, growth))
+    outfmt = "{}{} - {:12s} ({:.3f})"
+    print("\n" + message)
+    predictions = get_predictions(numbers)
+    index_closest = index_of_nearest([p for p, _, _ in predictions], last.value)
+    for i, (val, description, growth) in enumerate(predictions):
+        print(
+            outfmt.format(
+                closest_marker if i == index_closest else "  ", val, description, growth
+            )
+        )
 
 
-def plot_new_cases(data):
+def plot_new_cases(data, ax=None):
     num_only = [n.value for n in data]
     diffs = [n - n_prev for n_prev, n in zip(num_only, num_only[1:])]
     dates = [n.date for n in data[1:]]
 
     weekends = [d for d in dates if d.weekday() >= 5]
 
-    f = plt.figure(figsize=(12,7))
-    plt.plot_date(dates, diffs, c='b', label='new cases', marker='x')
-    plt.plot_date(dates, pd.Series(diffs).rolling(7).mean(), ls='dashed', marker='.', c='k', label='7 day RM')
-    plt.title('Cases added')
-    plt.xticks(dates)
-    plt.xlabel("Day")
-    plt.xticks(rotation=90)
-    ymin, ymax = plt.ylim()
-    plt.vlines(weekends, ymin, ymax, ls='dashed', label='weekend', colors='r', alpha=0.5)
-    plt.legend(loc='upper right')
+    if not ax:
+        f = plt.figure(figsize=(12, 7))
+        ax = plt.gca()
+    ax.plot_date(dates, diffs, c="b", label="new cases", marker="x")
+    ax.plot_date(
+        dates,
+        pd.Series(diffs).rolling(7).mean(),
+        ls="dashed",
+        marker=".",
+        c="k",
+        label="7 day RM",
+    )
+    ax.set_title("Cases added")
+    ax.set_xticks(dates)
+    ax.set_xlim(dates[0]-datetime.timedelta(days=1), dates[-1]+datetime.timedelta(days=1))
+    ax.tick_params(axis='x', labelrotation=90)
+    ymin, ymax = ax.get_ylim()
+    ax.vlines(
+        weekends, ymin, ymax, ls="dashed", label="weekend", colors="r", alpha=0.5
+    )
+    plt.legend(loc="upper right")
     plt.tight_layout()
-    f.savefig('corona_new_cases.png', dpi=300)
+    return plt.gcf()
 
 
-def plot_total_cases(data):
+def plot_total_cases(data, ax=None):
     num_only = [n.value for n in data]
     dates = [n.date for n in data]
-    weekends = [d for d in dates if d.weekday() >= 5]
 
-    f = plt.figure(figsize=(12,7))
-    plt.plot_date(dates, num_only, c='b', ls='solid', label='new cases', marker='x')
-    plt.title('Total cases')
-    plt.xticks(dates)
-    plt.xlabel("Day")
-    plt.xticks(rotation=90)
-    ymin, ymax = plt.ylim()
-    plt.vlines(weekends, ymin, ymax, ls='dashed', label='weekend', colors='r', alpha=0.5)
-    plt.legend(loc='upper right')
+    if not ax:
+        f = plt.figure(figsize=(12, 7))
+        ax = plt.gca()
+    ax.plot_date(dates, num_only, c="k", ls="solid", label="new cases", marker="x")
+    ax.set_title("Total cases")
+    ax.set_xticks(dates)
+    ax.tick_params(axis='x', labelrotation=90)
+    ax.set_xlim(dates[0]-datetime.timedelta(days=1), dates[-1]+datetime.timedelta(days=1))
+    ax.set_label("Day")
+    ymin, ymax = ax.get_ylim()
+
+    weekends = [d for d in dates if d.weekday() >= 5]
+    ax.vlines(
+        weekends, ymin, ymax, ls="dashed", label="weekend", colors="r", alpha=0.5
+    )
+    plt.legend(loc="upper right")
     plt.tight_layout()
-    f.savefig('corona_total_cases.png', dpi=300)
+    return plt.gcf()
 
 
 def main():
@@ -190,15 +212,18 @@ def main():
     print("LAST UPDATE\t", data[-1].date)
     print("...{}".format(history))
     print_predictions("predicted", data[:-1], data[-1], "* ")
-    print_predictions("next?", data, data[-1],"  ")
+    print_predictions("next?", data, data[-1], "  ")
 
     if added_today:
         with open(NUMBER_FILE, "w") as f_numbers:
             for entry in data:
                 print("{},{}".format(entry.date, entry.value), file=f_numbers)
         print("\nWrote new number to file")
-    plot_new_cases(data)
-    plot_total_cases(data)
+
+    f, (ax1, ax2) = plt.subplots(2, figsize=(12, 7), sharex=True)
+    f = plot_new_cases(data, ax1)
+    f = plot_total_cases(data, ax2)
+    f.savefig("corona.png", dpi=300)
 
 
 if __name__ == "__main__":
