ece3c0681b8af94901fb2ac4f604e3c2a69245d4
Chris Davison <c.jr.davison@gmail.com>
2020-07-07 08:45:29 +0100

    Simplify main of randomasmr

--- randomasmr/src/main.rs
+++ randomasmr/src/main.rs
@@ -1,32 +1,42 @@
-use shellexpand::tilde;
 use rand::random;
 use regex::Regex;
 use reqwest::blocking::get;
+use shellexpand::tilde;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn get_request_url() -> Result<String> {
     let token_path = tilde("~/.pinboard").to_string();
-    let pinboard_token = std::fs::read_to_string(token_path)?.trim().to_string(); 
+    let pinboard_token =
+        std::fs::read_to_string(token_path).map_err(|e| format!("Pinboard token: {}", e))?;
 
-	let pinboard_api_url = "https://api.pinboard.in/v1/posts/all?tag=asmr";
-    Ok(format!("{}&auth_token={}", pinboard_api_url, pinboard_token))
+    Ok(format!(
+        "https://api.pinboard.in/v1/posts/all?tag=asmr&auth_token={}",
+        pinboard_token.trim()
+    ))
 }
 
 fn get_videos_from_response(response: &str) -> Result<Vec<String>> {
-    let re = Regex::new(r#"href="(.+?)"\s"#).expect("Couldn't parse regex".into()); 
-    let caps: Vec<String> = re.captures_iter(response).map(|x| x[1].to_string()).collect();
+    let re = Regex::new(r#"href="(.+?)"\s"#).map_err(|_| "Failed to parse regex")?;
+    let caps: Vec<String> = re
+        .captures_iter(response)
+        .map(|x| x[1].to_string())
+        .collect();
     Ok(caps)
 }
 
 fn choose_random_video(videos: &[String]) -> String {
-    // let val = random::<usize>() % videos.len();
     videos[random::<usize>() % videos.len()].clone()
 }
 
 fn main() {
-    let pinboard_url = get_request_url().expect("Failed to read pinboard token");
-    let body = get(&pinboard_url).expect("API Request failed").text().expect("API request conversion to text failed");
-    let videos = get_videos_from_response(&body).unwrap();
-    println!("{}", choose_random_video(&videos));
+    let video = get_request_url()
+        .and_then(|url| get(&url).map_err(|_| "Couldn't GET".into()))
+        .and_then(|response| response.text().map_err(|_| "Couldn't read response".into()))
+        .and_then(|body| get_videos_from_response(&body).into())
+        .map(|videos| choose_random_video(&videos));
+    match video {
+        Ok(video) => println!("{}", video),
+        Err(e) => println!("{}", e),
+    };
 }
