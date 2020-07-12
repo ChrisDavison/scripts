7016cf10ae64eb64375e84ee1b9f2ce032f27332
Chris Davison <c.jr.davison@gmail.com>
2020-07-12 09:58:32 +0100

    Script to backup notes and recipes to git repo

--- backup_notes.py
+++ backup_notes.py
@@ -1,31 +1,69 @@
 #!/usr/bin/env python3
-"""Backup all notes (copy to another dir).
-
-May update this in future to also zip."""
+"""Backup all notes (copy to another dir)"""
+import os
+import contextlib
 from datetime import date
-
 from pathlib import Path
 
 
-def main(outdir, note_format="*.org"):
-    """Copy all notes with format FORMAT to outdir."""
-    dropbox = Path("~/Dropbox/").expand_user()
+@contextlib.contextmanager
+def working_directory(path):
+    """A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
 
-    files = [f for f in dropbox.rglob(note_format) if "dropbox.cache" not in str(f)]
-    files.extend([f for f in dropbox.rglob("*") if "assets" in str(f) and f.is_file()])
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(prev_cwd)
 
-    outdir = Path("~/Downloads/standalone-notes-backup/").expand_user()
 
-    for path in files:
-        out = outdir / path.relative_to(dropbox)
-        if not out.parent.exists():
-            out.parent.mkdir(parents=True)
-        if out.exists():
-            continue
-        out.write_bytes(path.read_bytes())
+def sync(input, output, exclude=['recipes'], msg=""):
+    input_files = {f.relative_to(input) for f in input.rglob('*.md')}
+    filename_has_exclude = lambda f: set(f.parts) == (set(f.parts) - set(exclude))
+    exclude = set(exclude)
+    output_files = set()
+    for filename in output.rglob('*.md'):
+        parts = set(filename.parts)
+        if len(parts) == len(parts - exclude):
+            output_files.add(filename.relative_to(output))
+    output_needing_deleted = output_files - input_files
+    input_being_added = input_files - output_files
+    print(f"{len(input_files)} files total", end='')
+    print(f" - {len(output_needing_deleted)} being removed,", end='')
+    print(f" {len(input_being_added)} being added")
+    files_to_copy = ' '.join(str(input / filename)
+            for filename in input_being_added)
+    files_to_remove = ' '.join(str(output / filename)
+            for filename in output_needing_deleted)
+    with working_directory(output):
+        today = date.today().strftime('%Y-%m-%d')
+        if output_needing_deleted:
+            for filename in output_needing_deleted:
+                    os.system(f"git rm {filename}")
+        if input_being_added:
+            os.system(f'rsync -v {files_to_copy} {output}')
+            os.system(f'git add --all')
+        if input_being_added or output_needing_deleted:
+            os.system(f'git commit -m "backup {msg}: {today}"')
+
+
+def main():
+    # Root directories
+    dir_dropbox = Path("~/Dropbox/").expanduser()
+    dir_repo = Path("~/code/knowledge/").expanduser()
+    dir_zips = dir_dropbox / "archive"
+    # Input directories
+    dir_notes = dir_dropbox / "notes"
+    dir_recipes = dir_dropbox / "recipes"
+    # Output directories
+    dir_notes_out = dir_repo
+    dir_recipes_out = dir_repo / "recipes"
+    print("Backing up notes")
+    sync(dir_notes, dir_notes_out, msg='notes')
+    print("\nBacking up recipes")
+    sync(dir_recipes, dir_recipes_out, msg='recipes', exclude=[])
 
 
 if __name__ == "__main__":
-    TODAY = date.today().strftime("%Y-%m-%d")
-    main(outdir=Path(f"~/Downloads/standalone-notes-backup--{TODAY}"))
-    print(f"Notes backed up to {outdir}")
+    main()
