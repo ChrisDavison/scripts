29786c18dcb54b86b1588c1bc6835d46d1ac3332
Chris Davison <c.jr.davison@gmail.com>
2020-08-26 15:58:41 +0100

    WIP rewrite of mdwiki
    Implemented view, and index. This will render markdown files, as well as
    list the directories and markdown directories underneath a path.
    
    Still has many bugs to iron out.

--- mdwiki-rs/src/main.rs
+++ mdwiki-rs/src/main.rs
@@ -1,57 +1,81 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-use std::path::PathBuf;
 use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
 
-use markdown;
-use rocket::request::{Form, FromForm};
-use rocket::response::{Flash, Redirect};
-use rocket::{get, post, routes};
-use rocket_contrib::templates::Template;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
 
-
-#[derive(FromForm)]
-pub struct Post {
-    contents: String,
-    title: String,
-}
-
-#[post("/save", data = "<post>")]
-fn save(post: Form<Post>) -> Flash<Redirect> {
-    if post.contents.is_empty() {
-        Flash::error(Redirect::to("/"), "Cannot be empty.")
-    } else {
-        Flash::success(
-            Redirect::to(format!("/view/{}.md", post.title)),
-            "Task added.",
-        )
+fn dir_contents_as_markdown_links(path: &PathBuf) -> String {
+    let mut files = String::new();
+    let mut dirs = String::new();
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let link_str = format!(
+            "\n- [{}](./view/{})",
+            path.file_name().unwrap().to_string_lossy(),
+            path.to_string_lossy()
+        );
+        if path.is_dir() {
+            dirs.push_str(link_str.as_ref())
+        } else {
+            if path.extension().unwrap().to_string_lossy() == "md" {
+                files.push_str(link_str.as_ref())
+            }
+        }
     }
+
+    format!(
+        "## directories\n\n{}\n\n---\n\n## files\n\n{}\n",
+        dirs, files
+    )
 }
 
-#[get("/view/<title>")]
-fn view(title: String) -> Template {
-    let p = PathBuf::from(&title);
+async fn index(_hb: web::Data<Handlebars<'_>>) -> impl Responder {
+    let contents = dir_contents_as_markdown_links(&PathBuf::from("."));
+    let html = markdown::to_html(&contents);
+    HttpResponse::Ok().body(html)
+}
+
+async fn edit(_hb: web::Data<Handlebars<'_>>, title: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Editing not implemented. Title: {}", title))
+}
+
+async fn view(hb: web::Data<Handlebars<'_>>, title: web::Path<String>) -> impl Responder {
+    let p = PathBuf::from(title.to_string());
     if p.exists() {
-        let contents = std::fs::read_to_string(p).unwrap();
-        let html = markdown::to_html(&contents);
         let mut context = HashMap::new();
-        context.insert("contents", html);
-        Template::render("view", &context)
+        if p.is_dir() {
+            let contents = dir_contents_as_markdown_links(&p);
+            let html = markdown::to_html(&contents);
+            context.insert("contents", html);
+        } else {
+            let contents = std::fs::read_to_string(p).unwrap();
+            let html = markdown::to_html(&contents);
+            context.insert("contents", html);
+        }
+        HttpResponse::Ok().body(hb.render("view", &context).unwrap())
     } else {
-        edit(title)
+        HttpResponse::Ok().body(format!("File not found: {}", title))
     }
 }
 
-#[get("/edit/<title>")]
-fn edit(title: String) -> Template {
-    let contents = format!("UNIMPLEMENTED editing {}", title);
-    let mut context = HashMap::new();
-    context.insert("contents", contents);
-    Template::render("view", &context)
-}
-
-fn main() {
-    rocket::ignite()
-        .mount("/", routes![save, edit, view,])
-        .attach(Template::fairing())
-        .launch();
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let view_source = include_str!("../templates/view.html");
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_string("view", view_source)
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .route("/", web::get().to(index))
+            .route("/view/{title}", web::get().to(view))
+            .route("/edit/{title}", web::get().to(edit))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
