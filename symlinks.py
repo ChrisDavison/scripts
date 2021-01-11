2f90ecd5f01f7b3319dc65c31ea7fdb4a9ed4ff4
Chris Davison <c.jr.davison@gmail.com>
2021-01-11 12:59:58 +0000

    Change formatting of symlinks script

--- symlinks.py
+++ symlinks.py
@@ -5,47 +5,49 @@ Prettyprint symlinks in the current directory.
 Basically, 'ls -lah | grep "\->"', with some formatting.
 """
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
+from collections import namedtuple
+
+TypedLink = namedtuple('TypedLink', 'linktype name')
+
+def get_links(root: Path) -> Tuple[List[Path], List[Path]]:
+    """
+    Get symlinks under root.
+    """
+    links_to_files = [p for p in Path(root).glob('*') if p.is_symlink() and p.is_file()]
+    links_to_dirs = [p for p in Path(root).glob('*') if p.is_symlink() and p.is_dir()]
+
+    return sorted(links_to_files), sorted(links_to_dirs)
 
 
-def print_links_relative_to_home(links: List[Path]):
-    """
-    Prettyprint symlinks, and replace home dir with '~'.
-    """
+def display_links(links: List[Path], preface: str):
+    if links:
+        to_fit = 60
+        remain = to_fit - (len(preface) + 2)
+        n_either_side = int(remain / 2)
+        spacer = "-" * n_either_side
+        print(spacer, preface.upper(), spacer)
+    width = max([len(str(l.name)) for l in links])
     home = Path('~').expanduser()
-    width = max([len(str(l)) for l in links])
+
     for link in links:
         actual = link.resolve()
         home_flag = ''
         if str(home) in str(actual):
             home_flag = '~/'
             actual = actual.relative_to(home)
-        print(f"{str(link):{width}}  |  {home_flag}{actual}")
-
+        print(f"{str(link):{width}}  >  {home_flag}{actual}")
 
 def main():
     """
     Print symlinks and the resolved target in a prettier layout.
     """
-    # Sort symlinks by dir-first, then alphabetically
-    # NOTE: dotfiles come first alphabetically
-    links = sorted([p for p in Path('.').glob('*') if p.is_symlink()], key=lambda p: (p.is_file(), p))
+    links_f, links_d = get_links('.')
+    display_links(links_f, "Files")
+    print()
+    display_links(links_d, "Directories")
 
-    if not links:
-        print("No symlinks.")
-        return
-    width = max([len(str(l)) for l in links])
-    home = Path('~').expanduser()
-
-    for link in links:
-        actual = link.resolve()
-        home_flag = ''
-        if str(home) in str(actual):
-            home_flag = '~/'
-            actual = actual.relative_to(home)
-        type = 'F' if link.is_file() else 'D'
-        print(f"{type} | {str(link):{width}} | {home_flag}{actual}")
-
+    
 
 if __name__ == "__main__":
     main()
