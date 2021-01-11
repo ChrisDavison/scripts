bb4f60e4d78cfc03adccd089b53baf687c3e5132
Chris Davison <c.jr.davison@gmail.com>
2021-01-11 19:20:41 +0000

    Add journal-format as arg to cal script

--- cal.py
+++ cal.py
@@ -6,7 +6,7 @@ import string
 from argparse import ArgumentParser
 
 
-def month_cal(year, month, start_day_of_week=0):
+def month_cal(year, month, start_day_of_week=0, indented=False):
     start_day_of_week = int(start_day_of_week) % 7
     today = datetime.date.today()
     year = int(year) if year else today.year
@@ -22,18 +22,40 @@ def month_cal(year, month, start_day_of_week=0):
     if week:
         by_weekday.append(week)
 
-    outstr = start.strftime("%B %Y").center(20) + "\n"
+    leading = '    ' if indented else ''
+    outstr = leading + start.strftime("%B %Y").center(20) + "\n"
     headers = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
     headerstr = " ".join(headers[start_day_of_week:] + headers[:start_day_of_week])
     N = len(headerstr)
     topbot_border = '+' + '-'*(N+2) + '+'
-    outstr += headerstr + "\n"
+    outstr += leading + headerstr + "\n"
     for i, week in enumerate(by_weekday):
         aligned_week = [str(w.day).rjust(2) for w in week]
         w = " ".join(aligned_week)
         justified = w.ljust(20) if i else w.rjust(20)
-        outstr += justified + "\n"
+        outstr += leading + justified + "\n"
     return outstr
+
+
+def make_journal(year, month):
+    today = datetime.date.today()
+    year = int(year) if year else today.year
+    month = int(month) if month else today.month
+    today = today.replace(year=year, month=month, day=1)
+    print(datetime.date(year, month, 1).strftime("# %Y-%m %B\n"))
+    print(month_cal(year, month, 0, indented=True))
+
+    print("\n## Recurring\n")
+    print("| Day | What |")
+    print("|-----|------|")
+    print("\n## Entries\n")
+    print("| Day  | What |")
+    print("|------|------|")
+    while today.month == month:
+        dow = 'MTWTFSS'[today.weekday()]
+        print(f"| {today.day:2d}.{dow} |      |")
+        today += datetime.timedelta(days=1)
+    print()
 
 
 if __name__ == "__main__":
@@ -44,15 +66,20 @@ if __name__ == "__main__":
     p.add_argument(
         "-s", "--startofweek", default=0, help="Day of week to start (0=Monday)"
     )
+    p.add_argument('-j', '--journal', help="Print in calendar journal-file format", default=False, action='store_true')
     args = p.parse_args()
     months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 
               'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     month = args.month
     if args.month and args.month[0] in string.ascii_letters:
         month = months.index(args.month[:3]) + 1
+
     if args.full_year:
         divider = "\n" + "-" * 80 + "\n\n"
         calendars = [month_cal(args.year, month, args.startofweek) for month in range(1, 13)]
         print(divider.join(calendars))
     else:
-        print(month_cal(args.year, month, args.startofweek))
+        if args.journal:
+            make_journal(args.year, args.month)
+        else:
+            print(month_cal(year, month, args.startofweek))
