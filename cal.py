04892c84b1a767a5315bf3c53f10be3308a1ee8f
Chris Davison <c.jr.davison@gmail.com>
2021-01-19 17:07:46 +0000

    Refactor calendar script

--- cal.py
+++ cal.py
@@ -1,84 +1,84 @@
 #!/usr/bin/env python3
 import datetime
+from typing import List, Iterator
 from argparse import ArgumentParser
+import textwrap
 
 
-def month_cal(year, month, *, indented=False, weekdays_only=False):
+WEEK = List[datetime.date]
+
+
+def justified_week_line(week: WEEK, weekdays_only: bool, justification: int) -> str:
+    """Justify the days in the current week, calendar style."""
+    days = [w.strftime("%2d") for w in week
+            if not weekdays_only
+            or (weekdays_only and w.weekday() < 5)]
+    return " ".join(days).rjust(justification)
+
+
+def day_iter(start, end=None) -> Iterator[datetime.date]:
+    """Iterate a day at a time.
+
+    If end is provided, stop without yielding end."""
+    while True:
+        yield start
+        start += datetime.timedelta(days=1)
+        if end and start >= end:
+            break
+
+
+def next_month(now: datetime.date) -> datetime.date:
+    """Get the month after this one."""
+    if now.month == 12:
+        return now.replace(year=now.year+1, month=1)
+    return now.replace(month=now.month+1)
+
+
+def cal(year: int, month: int, *,
+        indented: bool = False,
+        weekdays_only: bool = False) -> List[str]:
+    """Generate a calendar."""
     start = datetime.date(year, month, 1)
-    by_weekday, week, temp = [], [], start
-    while temp.month == start.month:
-        if temp.weekday() == 0 and week:
-            by_weekday.append(week)
-            week = []
-        if not weekdays_only:
-            week.append(temp)
-        elif temp.weekday() not in [5, 6]:
-            week.append(temp)
-        else:
-            # Weekdays only and today is sat or sun
-            pass
-        temp += datetime.timedelta(days=1)
-    if week:
-        by_weekday.append(week)
+    weeks: List[WEEK] = [[]]
+    week: WEEK = []
+    for day in day_iter(start, next_month(start)):
+        weeks[len(weeks)-1].append(day)
+        if day.weekday() == 6:
+            weeks.append([])
 
-    leading = '    ' if indented else ''
     headers = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
     if weekdays_only:
-        headers = ["Mo", "Tu", "We", "Th", "Fr"]
-    headerstr = " ".join(headers)
-    just = 20 if not weekdays_only else 14
-    out = []
-    out.append(leading + start.strftime("%B %Y").center(just))
-    out.append(leading + headerstr)
-    for i, week in enumerate(by_weekday):
-        aligned_week = [str(w.day).rjust(2) for w in week]
-        w = " ".join(aligned_week)
-        justified = w.ljust(just) if i else w.rjust(just)
-        out.append(leading + justified)
+        headers = headers[:-2]
+
+    justification = len(headers) * 2 + len(headers) - 1
+    out = [
+        start.strftime("%B %Y").center(justification),
+        " ".join(headers)
+    ]
+    for week in weeks:
+        out.append(justified_week_line(week, weekdays_only, justification))
+    if indented:
+        return textwrap.indent('\n'.join(out), prefix='    ').splitlines()
     return out
-
-
-def make_journal(year, month):
-    today = datetime.date.today()
-    year = int(year) if year else today.year
-    month = int(month) if month else today.month
-    today = today.replace(year=year, month=month, day=1)
-    print(datetime.date(year, month, 1).strftime("# %Y-%m %B\n"))
-    print(month_cal(year, month, 0, indented=True))
-
-    print("\n## Recurring\n")
-    print("| Day | What |")
-    print("|-----|------|")
-    print("\n## Entries\n")
-    print("| Day  | What |")
-    print("|------|------|")
-    while today.month == month:
-        dow = 'MTWTFSS'[today.weekday()]
-        print(f"| {today.day:2d}.{dow} |      |")
-        today += datetime.timedelta(days=1)
-    print()
-
-
-def main(year, month, full_year, weekdays_only):
-    if args.full_year:
-        divider = "\n" + "-" * 80 + "\n\n"
-        calendars = ['\n'.join(month_cal(year, month))
-                     for month in range(1, 13)]
-        print(divider.join(calendars))
-    else:
-        print('\n'.join(month_cal(year, month, weekdays_only=weekdays_only)))
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("-y", "--year",
-                   default=datetime.date.today().year)
+                   default=datetime.date.today().year, type=int)
     p.add_argument("-m", "--month",
-                   default=datetime.date.today().month)
+                   default=datetime.date.today().month, type=int)
     p.add_argument("-Y", "--full-year",
                    help="Generate calendar for every month",
-                   action='store_true',)
-    p.add_argument("-w", "--weekdays",
-                   help="Only show weekdays", action='store_true')
+                   action='store_true')
+    p.add_argument("-w", "--weekdays-only",
+                   help="Only show weekdays",
+                   action='store_true')
+
     args = p.parse_args()
-    main(args.year, args.month, args.full_year, args.weekdays)
+    months = list(range(1, 13)) if args.full_year else [args.month]
+    calendars = [
+        '\n'.join(cal(args.year, month, weekdays_only=args.weekdays_only))
+        for month in months
+    ]
+    print('\n\n'.join(calendars))
