40dd18cfde1121e2002fdc071cf6c0b0aeb5aa72
Chris Davison <c.jr.davison@gmail.com>
2021-12-04 12:01:02 +0000

    change display format

--- hoffman_coffee.py
+++ hoffman_coffee.py
@@ -2,18 +2,28 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-v", "--volume", type=int, default=300)
-parser.add_argument("-r", "--ratio", type=int, help="Grams per litre", default=60)
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", metavar="VOLUME", type=int)
+group.add_argument("-g", metavar="GRAMS", type=int)
+parser.add_argument("-r", metavar="ML_PER_GRAM", type=int, default=16.7)
 args = parser.parse_args()
 
-ml = args.volume
-grams = args.ratio * ml / 1000
+print(f"Hoffman v60 Method")
+if args.volume:
+    ml = round(args.volume, 2)
+    grams = int(ml / args.ratio)
+elif args.grams:
+    grams = args.grams
+    ml = int(args.ratio * grams)
+else:
+    ml = 300
+    grams = 18
+
 bloom = grams * 2
 first = ml * 0.6
 second = ml * 0.4
 
-print(f"Hoffman v60 Method - {ml}ml of coffee ({args.volume / grams:.1f}:1)")
-print(f"Coffee: {grams}g (then zero/tare)")
-print(f"Bloom & swirl: {bloom}ml")
-print(f"1st pour: up to {first}ml")
-print(f"2nd pour: another {second}ml")
+print(f"{ml:.0f}ml → {grams}g ({ml / grams:.1f}:1)\n")
+print(f"Bloom & swirl  {bloom:.0f}ml")
+print(f"1st pour       {bloom:.0f} → {first:.0f}ml")
+print(f"2nd pour       {first:.0f} → {first+second:.0f}ml")
