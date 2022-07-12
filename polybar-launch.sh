2a94d2471486dca28a84a2104ee1c58bc50ff26c
Chris Davison <c.jr.davison@gmail.com>
2022-07-12 16:08:17 +0100

    Scropt to launch dual polybars

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 pkill polybar
-for m in `polybar --list-monitors | cut -d':' -f1`; do
-    MONITOR=$m polybar -r internal &
-done
+# for m in `polybar --list-monitors | cut -d':' -f1`; do
+#     MONITOR=$m polybar -r internal &
+# done
+MONITOR=eDP-1 polybar -r internal &
+MONITOR=DP-1 polybar -r external &
