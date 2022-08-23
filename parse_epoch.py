53e9305a22c6ff55918669b2cb1a9a437c2db9b8
Chris Davison <c.jr.davison@gmail.com>
2022-08-23 10:32:50 +0100

    Fix typo

--- parse_epoch.py
+++ parse_epoch.py
@@ -8,6 +8,4 @@ parser = ArgumentParser("parse_epoch")
 parser.add_argument("epoch", type=int)
 epoch = parser.parse_args().epoch
 
-parser.
-
 print(datetime.datetime.fromtimestamp(int(epoch)).strftime("%Y-%m-%d %H:%M:%S"))
